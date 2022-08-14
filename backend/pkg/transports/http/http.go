61a2920b14c7b5a2c24d5b5aed2ffc19ae2f33cb
Barnaby Keene <accounts@southcla.ws>
2022-08-14 16:18:37 +0100

    more docs

--- backend/pkg/transports/http/http.go
+++ backend/pkg/transports/http/http.go
@@ -1,3 +1,14 @@
+// Package http provides a transport layer using HTTP. In this layer, most of
+// the code is generated: low level HTTP handlers, request and response structs
+// and object validation.
+//
+// This is wired up to the service layer using "Bindings" which are just glue
+// code which call service APIs from the endpoint handlers and deal with errors.
+//
+// Within this package, all you will find is fx providers for the actual HTTP
+// server, the router and the bindings. The bindings package is where most of
+// the logic is and it depends on the router to mount the bindings.
+//
 package http
 
 import (
--- backend/pkg/transports/transports.go
+++ backend/pkg/transports/transports.go
@@ -1,3 +1,6 @@
+// Package transport contains all the transport layers that facilitate
+// interfacing with the application. The main transport method is HTTP which is
+// implemented using code generated from an OpenAPI specification.
 package transport
 
 import (
