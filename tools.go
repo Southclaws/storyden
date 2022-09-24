1af1846e5aadd72fca86962084562a2199113deb
Barnaby Keene <accounts@southcla.ws>
2022-09-24 14:43:06 +0100

    fix tools import

--- tools.go
+++ tools.go
@@ -3,7 +3,7 @@ package storyden
 //go:build+ tools
 
 import (
-	_ "entgo.io/ent/cmd/ent"
+	_ "entgo.io/ent"
 	_ "github.com/99designs/gqlgen"
 	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
 )
