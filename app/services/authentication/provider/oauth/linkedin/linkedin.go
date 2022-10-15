404dcef958d86aa546c6d0dcd0a13a86cf5efca0
Barnaby Keene <accounts@southcla.ws>
2022-10-15 13:11:29 +0100

    add avatar download for linkedin provider

--- app/services/authentication/provider/oauth/linkedin/linkedin.go
+++ app/services/authentication/provider/oauth/linkedin/linkedin.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"4d63.com/optional"
 	"github.com/Southclaws/fault/errctx"
 	"github.com/go-resty/resty/v2"
 	"github.com/pkg/errors"
@@ -14,6 +15,7 @@ import (
 	"github.com/Southclaws/storyden/app/resources/account"
 	"github.com/Southclaws/storyden/app/resources/authentication"
 	"github.com/Southclaws/storyden/app/services/authentication/provider/oauth/all"
+	"github.com/Southclaws/storyden/app/services/avatar"
 	"github.com/Southclaws/storyden/internal/config"
 )
 
@@ -26,12 +28,13 @@ const (
 type LinkedInProvider struct {
 	auth_repo    authentication.Repository
 	account_repo account.Repository
+	avatar_svc   avatar.Service
 
 	callback string
 	config   all.Configuration
 }
 
-func New(cfg config.Config, auth_repo authentication.Repository, account_repo account.Repository) (*LinkedInProvider, error) {
+func New(cfg config.Config, auth_repo authentication.Repository, account_repo account.Repository, avatar_svc avatar.Service) (*LinkedInProvider, error) {
 	config, err := all.LoadProvider(id)
 	if err != nil {
 		return nil, err
@@ -40,6 +43,7 @@ func New(cfg config.Config, auth_repo authentication.Repository, account_repo ac
 	return &LinkedInProvider{
 		auth_repo:    auth_repo,
 		account_repo: account_repo,
+		avatar_svc:   avatar_svc,
 		config:       config,
 		callback:     all.Redirect(cfg, id),
 	}, nil
@@ -101,44 +105,104 @@ func (p *LinkedInProvider) Login(ctx context.Context, state, code string) (*acco
 		return nil, errctx.Wrap(errors.New("no access token in response"), ctx)
 	}
 
+	// Use the auth token for all future requests
+	rest.SetAuthToken(auth.AccessToken)
+
 	var profile struct {
-		ID        string `json:"id"`
-		FirstName string `json:"localizedFirstName"`
-		LastName  string `json:"localizedLastName"`
+		ID             string `json:"id"`
+		FirstName      string `json:"localizedFirstName"`
+		LastName       string `json:"localizedLastName"`
+		ProfilePicture struct {
+			DisplayImage struct {
+				Elements []struct {
+					Identifiers []struct {
+						Identifier string `json:"identifier"`
+					} `json:"identifiers"`
+				} `json:"elements"`
+			} `json:"displayImage~"`
+		} `json:"profilePicture"`
 	}
 
 	_, err = rest.R().
-		SetAuthToken(auth.AccessToken).
 		SetResult(&profile).
-		Get("https://api.linkedin.com/v2/me")
+		Get("https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))")
 	if err != nil {
 		return nil, errctx.Wrap(err, ctx)
 	}
 
+	// TODO: Iterate "elements" and search for the largest avatar instead of
+	// just picking the first item from the list.
+	avatarURL := optional.OfPtr(func() *string {
+		if len(profile.ProfilePicture.DisplayImage.Elements) > 0 && len(profile.ProfilePicture.DisplayImage.Elements[0].Identifiers) > 0 {
+			return &profile.ProfilePicture.DisplayImage.Elements[0].Identifiers[0].Identifier
+		}
+		return nil
+	}())
+
+	// TODO: Invent a better handle generator
+	handle := fmt.Sprintf("%s-%s-%d", profile.FirstName, profile.LastName, time.Now().Day())
+
+	name := fmt.Sprint(profile.FirstName, " ", profile.LastName)
+
 	// TODO: Everything below this can be made generic for all OAuth providers.
 
-	authmethod, exists, err := p.auth_repo.LookupByIdentifier(ctx, "linkedin", profile.ID)
+	acc, err := p.getOrCreateAccount(ctx, "linkedin", profile.ID, auth.AccessToken, handle, name)
 	if err != nil {
 		return nil, errctx.Wrap(err, ctx)
+	}
+
+	if err := p.setAvatar(ctx, rest, acc, avatarURL); err != nil {
+		// failing to set the avatar is not a big issue.
+		fmt.Println(err)
+	}
+
+	return acc, nil
+}
+
+func (p *LinkedInProvider) getOrCreateAccount(ctx context.Context, provider authentication.Service, identifier, token, handle, name string) (*account.Account, error) {
+	authmethod, exists, err := p.auth_repo.LookupByIdentifier(ctx, provider, identifier)
+	if err != nil {
+		return nil, errctx.Wrap(errors.Wrap(err, "failed to lookup existing account"), ctx)
 	}
 
 	if exists {
 		return &authmethod.Account, nil
 	}
 
-	// TODO: Invent a better handle generator
-	handle := fmt.Sprintf("%s-%s-%d", profile.FirstName, profile.LastName, time.Now().Day())
-
 	acc, err := p.account_repo.Create(ctx, handle,
-		account.WithName(fmt.Sprint(profile.FirstName, " ", profile.LastName)))
+		account.WithName(name))
 	if err != nil {
-		return nil, errctx.Wrap(err, ctx)
+		return nil, errctx.Wrap(errors.Wrap(err, "failed to create new account"), ctx)
 	}
 
-	_, err = p.auth_repo.Create(ctx, acc.ID, "linkedin", profile.ID, auth.AccessToken, nil)
+	_, err = p.auth_repo.Create(ctx, acc.ID, provider, identifier, token, nil)
 	if err != nil {
 		return nil, errctx.Wrap(errors.Wrap(err, "failed to create new auth method for account"), ctx)
 	}
 
 	return acc, nil
+}
+
+func (p *LinkedInProvider) setAvatar(ctx context.Context, rest *resty.Client, acc *account.Account, avatarURL optional.Optional[string]) error {
+	url, ok := avatarURL.Get()
+	if !ok {
+		return nil
+	}
+
+	avatarBinary, err := rest.R().
+		SetDoNotParseResponse(true).
+		Get(url)
+	if err != nil {
+		return errctx.Wrap(err, ctx)
+	}
+
+	if avatarBinary == nil {
+		return nil
+	}
+
+	if err := p.avatar_svc.Set(ctx, acc.ID, avatarBinary.RawBody()); err != nil {
+		return errctx.Wrap(err, ctx)
+	}
+
+	return nil
 }
