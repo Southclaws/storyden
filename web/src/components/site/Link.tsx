16e6ddf988121623ad4707960b11c3a021181c46
Barnaby Keene <accounts@southcla.ws>
2022-12-13 20:25:01 +0100

    fix link after next update

--- web/src/components/site/Link.tsx
+++ web/src/components/site/Link.tsx
@@ -4,7 +4,14 @@ import NextLink from "next/link";
 export default function Link({ children, href, ...rest }: LinkProps) {
   return (
     <ChakraLink {...rest}>
-      <NextLink href={href!}>{children}</NextLink>
+      <NextLink
+        // we use legacy behaviour since it's the only thing that
+        // seems to make it compatible with Chakra's link component.
+        legacyBehavior
+        href={href!}
+      >
+        {children}
+      </NextLink>
     </ChakraLink>
   );
 }
