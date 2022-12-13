2bcf0d86d7ca84f1411b8e1f99abf8d59821e1fe
Barnaby Keene <accounts@southcla.ws>
2022-12-13 13:00:57 +0100

    throw together a basic navbar

---
+++ web/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import { Box, HStack } from "@chakra-ui/react";
+import { Profile } from "../Profile/Profile";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { StorydenLogo } from "../StorydenLogo";
+
+export function Navigation() {
+  return (
+    <HStack width="full" height="4em" justifyContent="center" bgColor="#E5E5E5">
+      <HStack width="full" maxW="container.lg" justifyContent="space-around">
+        <StorydenLogo />
+
+        <SearchBar />
+
+        <Profile />
+      </HStack>
+    </HStack>
+  );
+}
