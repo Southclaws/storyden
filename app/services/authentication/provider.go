b5a9548e86cd7df90b12e948bac6f5985b225945
Barnaby Keene <accounts@southcla.ws>
2022-12-13 10:56:19 +0000

    some documentation around auth providers

--- app/services/authentication/provider.go
+++ app/services/authentication/provider.go
@@ -18,14 +18,21 @@ type Provider interface {
 	// of environment variable configuration for each provider.
 	Enabled() bool
 
-	// Provider general information.
+	// Provider general information. This could be a struct but it's simpler as
+	// part of the interface for now, despite bloating the interface a bit.
 	Name() string
 	ID() string
 	LogoURL() string
 
-	// Link is the actual login link to show to users.
+	// Link will, for providers that support it, provide a URL to a third-party
+	// authenticator. OAuth providers use this to start the authentication flow.
 	Link() string
 
-	// Login is the callback function.
-	Login(ctx context.Context, state, code string) (*account.Account, error)
+	// Login is a function that will validate and authenticate a user given that
+	// the provider is happy with the input. The input format differs depending
+	// on the provider. For example, an OAuth provider will use `state` and
+	// `secret` as the `state` and `code` components of the OAuth2 specification
+	// and a simple password-based provider may simply want the account handle
+	// in the `state` and their password in the `secret`.
+	Login(ctx context.Context, state, secret string) (*account.Account, error)
 }
