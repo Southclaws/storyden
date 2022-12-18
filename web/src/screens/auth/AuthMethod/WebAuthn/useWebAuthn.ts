a6bb1dc99ea66d56eff5c6b255f64ea784907203
Barnaby Keene <accounts@southcla.ws>
2022-12-18 10:38:54 +0100

    use simplewebauthn for browser webauthn flow

--- web/src/screens/auth/AuthMethod/WebAuthn/useWebAuthn.ts
+++ web/src/screens/auth/AuthMethod/WebAuthn/useWebAuthn.ts
@@ -4,13 +4,11 @@ import {
   webAuthnRequestCredential,
 } from "src/api/openapi/auth";
 
-import {
-  RegistrationCredential,
-  RegistrationCredentialJSON,
-} from "@simplewebauthn/typescript-types";
+import { startRegistration } from "@simplewebauthn/browser";
+import { PublicKeyCredentialCreationOptionsJSON } from "@simplewebauthn/typescript-types";
 
 export function useWebAuthn() {
-  const { register, handleSubmit, formState } = useForm({
+  const { register, handleSubmit } = useForm({
     //
   });
 
@@ -19,74 +17,23 @@ export function useWebAuthn() {
   }: FieldValues) => {
     const response = await webAuthnRequestCredential(username);
 
-    const { publicKey } = response;
-    if (!publicKey) {
-      throw new Error("publicKey is empty");
-    }
+    // TODO: OpenAPI spec for WebAuthn requests and responses.
+    const publicKey = response[
+      "publicKey"
+    ] as PublicKeyCredentialCreationOptionsJSON;
 
-    const publicKeyCredentialCreationOptions = {
+    const credential = await startRegistration({
       ...publicKey,
+      excludeCredentials: [],
+    });
 
-      rp: {
-        id: "localhost", // TODO: Configure self-domain or get from API.
-        name: "Storyden",
-      },
+    const creds = await webAuthnMakeCredential(credential);
 
-      // overwrite challenge and user with the correct format
-      challenge: Uint8Array.from(publicKey.challenge as string, (c) =>
-        c.charCodeAt(0)
-      ),
-      user: {
-        id: Uint8Array.from(publicKey.user.id as string, (c) =>
-          c.charCodeAt(0)
-        ),
-        name: publicKey.user.name,
-        displayName: publicKey.user.displayName,
-      },
-    };
-
-    const credential = (await navigator.credentials.create({
-      publicKey: publicKeyCredentialCreationOptions,
-    })) as RegistrationCredential; // cast required as TS is outdated currently.
-
-    if (credential == null) {
-      throw new Error("credential was null after create");
-    }
-
-    const makeCredentialBody: RegistrationCredentialJSON = {
-      id: credential?.id,
-      rawId: bufToBase64(credential.rawId),
-      type: credential.type,
-      response: {
-        clientDataJSON: bufToBase64(credential.response.clientDataJSON),
-        attestationObject: bufToBase64(credential.response.attestationObject),
-      },
-      clientExtensionResults: credential.getClientExtensionResults(),
-      authenticatorAttachment: credential.authenticatorAttachment,
-    };
-
-    console.log({ credential, makeCredentialBody });
-
-    const creds = await webAuthnMakeCredential(makeCredentialBody);
-
-    console.log({ creds });
+    console.info("webauthn success", { accountID: creds.id });
   };
 
   return {
     register,
     onSubmit: handleSubmit(onSubmit),
   };
-}
-
-function bufToBase64(buffer: ArrayBuffer): string {
-  const bytes = new Uint8Array(buffer);
-  let str = "";
-
-  for (const charCode of bytes) {
-    str += String.fromCharCode(charCode);
-  }
-
-  const base64String = btoa(str);
-
-  return base64String.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
 }
