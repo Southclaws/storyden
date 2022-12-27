3103aa09172201f21b138acd65e680cb326ba562
Barnaby Keene <accounts@southcla.ws>
2022-12-27 16:48:39 +0000

    fix api rewrite

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_ADDRESS}/api/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
