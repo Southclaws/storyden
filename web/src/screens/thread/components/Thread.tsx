00c83802c18cd2b94706067460f2f109d5dfb841
Barnaby Keene <accounts@southcla.ws>
2023-01-02 15:18:26 +0000

    move postlist into its own file and add some gap between them

--- web/src/screens/thread/components/Thread.tsx
+++ web/src/screens/thread/components/Thread.tsx
@@ -1,19 +1,7 @@
-import { Box, Heading, ListItem, OrderedList, VStack } from "@chakra-ui/react";
-import { Post, Thread } from "src/api/openapi/schemas";
+import { Heading, VStack } from "@chakra-ui/react";
+import { Thread } from "src/api/openapi/schemas";
 import { CategoryPill } from "src/components/CategoryPill";
-import { PostView } from "./Post";
-
-export function PostListView(props: { posts: Post[] }) {
-  return (
-    <OrderedList gap={2} display="flex" flexDir="column" width="full">
-      {props.posts.map((p) => (
-        <ListItem key={p.id} listStyleType="none" m={0}>
-          <PostView {...p} />
-        </ListItem>
-      ))}
-    </OrderedList>
-  );
-}
+import { PostListView } from "./PostList";
 
 export function ThreadView(props: Thread) {
   return (
