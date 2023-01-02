3bb89614a9162b33831c1cd4339e2a17cf430c6f
Barnaby Keene <accounts@southcla.ws>
2023-01-02 12:54:15 +0000

    add home link to nav

---
+++ web/src/components/Navigation/HomeLink.tsx
@@ -0,0 +1,10 @@
+import Link from "../site/Link";
+import { StorydenLogo } from "../StorydenLogo";
+
+export function HomeLink() {
+  return (
+    <Link href="/">
+      <StorydenLogo />
+    </Link>
+  );
+}
