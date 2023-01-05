b64c2eaaae229af746ab3e6e67b9ef5ccbd7dfa4
Barnaby Keene <accounts@southcla.ws>
2023-01-05 07:58:43 +0000

    fix anchor

--- web/src/components/site/Link.tsx
+++ web/src/components/site/Link.tsx
@@ -3,15 +3,13 @@ import NextLink from "next/link";
 
 export default function Link({ children, href, ...rest }: LinkProps) {
   return (
-    <ChakraLink {...rest}>
-      <NextLink
-        // we use legacy behaviour since it's the only thing that
-        // seems to make it compatible with Chakra's link component.
-        legacyBehavior
-        href={href!}
-      >
-        {children}
-      </NextLink>
-    </ChakraLink>
+    <NextLink
+      // we use legacy behaviour since it's the only thing that
+      // seems to make it compatible with Chakra's link component.
+      legacyBehavior
+      href={href!}
+    >
+      <ChakraLink {...rest}>{children}</ChakraLink>
+    </NextLink>
   );
 }
