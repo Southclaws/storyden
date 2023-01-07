a6865dd41491f5b659ea5e61fe093ffaf118b27d
Barnaby Keene <accounts@southcla.ws>
2023-01-07 15:01:21 +0000

    spice up the auth screen

--- web/src/screens/auth/components/AuthBox.tsx
+++ web/src/screens/auth/components/AuthBox.tsx
@@ -1,17 +1,9 @@
-import { Box } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { Box, BoxProps } from "@chakra-ui/react";
 
-export function AuthBox({ children }: { children: ReactNode }) {
+export function AuthBox(props: BoxProps) {
   return (
-    <Box
-      id="AuthBox"
-      p={6}
-      bg="linear-gradient(141.91deg, #B7CEF1 0%, #2FD596 99.55%)"
-      borderRadius={8}
-      width="full"
-      maxW="xs"
-    >
-      {children}
+    <Box id="AuthBox" p={6} borderRadius={8} width="full" maxW="xs" {...props}>
+      {props.children}
     </Box>
   );
 }
