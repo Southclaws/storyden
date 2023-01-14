7d74cc7162a34bea95b9078258fe99c2e2d7b7f3
Barnaby Keene <accounts@southcla.ws>
2023-01-14 13:36:05 +0000

    fuck cookies

--- web/src/pages/logout.tsx
+++ web/src/pages/logout.tsx
@@ -1,10 +1,18 @@
 import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import nookies from "nookies";
+import { useEffect } from "react";
 
 export default function Page() {
-  document.cookie =
-    "storyden-session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  const router = useRouter();
+  useEffect(() => {
+    document.cookie =
+      "storyden-session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+    router.push("/");
+  }, [router]);
+
   return <Link href="/login">Logged out. Returning to login.</Link>;
 }
 
@@ -16,9 +24,5 @@ export async function getServerSideProps(
   nookies.destroy(ctx, "storyden-session");
   return {
     props: {},
-    redirect: {
-      destination: "/",
-      permanent: false,
-    },
   };
 }
