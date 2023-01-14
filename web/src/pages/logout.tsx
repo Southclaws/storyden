8e9b43515ddd6d71fbea53ea020d8f35bc6f788b
Barnaby Keene <accounts@southcla.ws>
2023-01-14 13:42:37 +0000

    undo all that bullshit and try with quotes on clear site data

--- web/src/pages/logout.tsx
+++ web/src/pages/logout.tsx
@@ -1,18 +1,7 @@
 import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import nookies from "nookies";
-import { useEffect } from "react";
 
 export default function Page() {
-  const router = useRouter();
-  useEffect(() => {
-    document.cookie =
-      "storyden-session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-
-    router.push("/");
-  }, [router]);
-
   return <Link href="/login">Logged out. Returning to login.</Link>;
 }
 
@@ -20,9 +9,12 @@ export async function getServerSideProps(
   ctx: GetServerSidePropsContext
 ): Promise<GetServerSidePropsResult<{}>> {
   ctx.res.setHeader("Clear-Site-Data", `"cookies"`);
-  ctx.res.setHeader("Set-Cookie", "storyden-session=x; Max-Age=0");
-  nookies.destroy(ctx, "storyden-session");
+
   return {
     props: {},
+    redirect: {
+      destination: "/",
+      permanent: false,
+    },
   };
 }
