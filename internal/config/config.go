0b989abe72bac91dc1a69a0138df440435ce10d8
Barnaby Keene <accounts@southcla.ws>
2023-03-30 21:07:46 +0100

    remove unused config values

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,14 +13,11 @@ type Config struct {
 	Production bool          `envconfig:"PRODUCTION" default:"false"`
 	LogLevel   zapcore.Level `envconfig:"LOG_LEVEL"  default:"info"`
 
-	DatabaseURL            string `envconfig:"DATABASE_URL"           required:"true"`
-	MailTemplatesDirectory string `envconfig:"MAIL_TEMPLATES_DIRECTORY" default:"email"`
-	ListenAddr             string `envconfig:"LISTEN_ADDR"            default:"0.0.0.0:8000"`
-	CookieDomain           string `envconfig:"COOKIE_DOMAIN"          default:"localhost"`
-	SessionKey             string `envconfig:"SESSION_KEY"            required:"true"`
-	PublicWebAddress       string `envconfig:"PUBLIC_WEB_ADDRESS"     default:"http://localhost:3000"`
-	PublicApiAddress       string `envconfig:"PUBLIC_API_ADDRESS"     default:"http://localhost:8000"`
-	AmqpAddress            string `envconfig:"AMQP_ADDRESS"           default:"amqp://rabbit:5672"`
+	DatabaseURL      string `envconfig:"DATABASE_URL"           required:"true"`
+	ListenAddr       string `envconfig:"LISTEN_ADDR"            default:"0.0.0.0:8000"`
+	CookieDomain     string `envconfig:"COOKIE_DOMAIN"          default:"localhost"`
+	SessionKey       string `envconfig:"SESSION_KEY"            required:"true"`
+	PublicWebAddress string `envconfig:"PUBLIC_WEB_ADDRESS"     default:"http://localhost:3000"`
 
 	S3Endpoint  string `envconfig:"S3_ENDPOINT"   required:"true"`
 	S3Bucket    string `envconfig:"S3_BUCKET"     required:"true"`
