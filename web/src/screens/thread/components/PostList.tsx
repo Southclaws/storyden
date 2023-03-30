96aa57448bd1645195e499ea27f6a310da15d668
Barnaby <accounts@southcla.ws>
2023-03-30 19:16:44 +0100

    add dividers between posts

--- web/src/screens/thread/components/PostList.tsx
+++ web/src/screens/thread/components/PostList.tsx
@@ -1,14 +1,17 @@
-import { OrderedList, ListItem } from "@chakra-ui/react";
+import { OrderedList, ListItem, Divider } from "@chakra-ui/react";
 import { Post } from "src/api/openapi/schemas";
 import { PostView } from "./Post";
 
 export function PostListView(props: { posts: Post[] }) {
   return (
-    <OrderedList gap={6} display="flex" flexDir="column" width="full">
+    <OrderedList gap={4} display="flex" flexDir="column" width="full">
       {props.posts.map((p) => (
-        <ListItem key={p.id} listStyleType="none" m={0}>
-          <PostView {...p} />
-        </ListItem>
+        <>
+          <Divider />
+          <ListItem key={p.id} listStyleType="none" m={0}>
+            <PostView {...p} />
+          </ListItem>
+        </>
       ))}
     </OrderedList>
   );
