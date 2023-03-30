6cdeb4a49476e36dae30b039af231c487c3e64cb
Barnaby <accounts@southcla.ws>
2023-03-30 18:52:50 +0100

    spice up the navitem and make it properly accessible

--- web/src/components/Sidebar/Sidebar.tsx
+++ web/src/components/Sidebar/Sidebar.tsx
@@ -1,32 +1,40 @@
-import { Box, List } from "@chakra-ui/react";
+import { Heading, HStack, List, VStack } from "@chakra-ui/react";
+import { HomeIcon } from "@heroicons/react/24/outline";
 import { map } from "lodash/fp";
 import { Category } from "src/api/openapi/schemas";
 import { Unready } from "../Unready";
 import { NavItem } from "./components/NavItem";
 import { useSidebar } from "./useSidebar";
 
-const mapCategories = (selected?: string) =>
-  map((c: Category) => (
-    <NavItem
-      key={c.id}
-      href={`/c/${c.name}`}
-      w="full"
-      selected={c.name === selected}
-    >
+const mapCategories = map((c: Category) => (
+  <NavItem key={c.id} href={`/c/${c.name}`} w="full">
+    <Heading size="sm" role="navigation" variant="ghost" w="full">
       {c.name}
-    </NavItem>
-  ));
+    </Heading>
+  </NavItem>
+));
 
 export function Sidebar() {
-  const { error, categories, category } = useSidebar();
+  const { error, categories } = useSidebar();
 
   if (error) return <Unready {...error} />;
 
   return (
-    <Box as="nav" py={4}>
-      <List margin={0} display="flex" flexDirection="column" gap={2}>
-        {mapCategories(category)(categories)}
+    <VStack as="nav" py={4} gap={4} alignItems="start">
+      <HStack gap={2}>
+        <NavItem href="/">
+          <HomeIcon width="1.5em" />
+        </NavItem>
+      </HStack>
+      <List
+        margin={0}
+        display="flex"
+        flexDirection="column"
+        gap={2}
+        width="full"
+      >
+        {mapCategories(categories)}
       </List>
-    </Box>
+    </VStack>
   );
 }
