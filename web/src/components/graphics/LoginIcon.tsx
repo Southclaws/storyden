e56eb93a2255f0add8b64ee941a95dc206681604
Barnaby Keene <accounts@southcla.ws>
2023-04-15 21:48:07 +0100

    tweak login icon layout a bit

--- web/src/components/graphics/LoginIcon.tsx
+++ web/src/components/graphics/LoginIcon.tsx
@@ -2,13 +2,7 @@ import { SVGProps } from "react";
 
 export function LoginIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={24}
-      height={24}
-      fill="none"
-      {...props}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props}>
       <path
         stroke="#141414"
         strokeLinecap="round"
