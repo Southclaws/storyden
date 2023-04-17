e29a3de1ccb4ee1e6472283d4a8b24552fc02178
Barnaby Keene <accounts@southcla.ws>
2023-04-17 21:24:20 +0100

    close the dashboard upon navigating to another page

--- web/src/components/Navigation/Navpill/useNavpill.ts
+++ web/src/components/Navigation/Navpill/useNavpill.ts
@@ -1,11 +1,17 @@
 import { useOutsideClick } from "@chakra-ui/react";
-import { RefObject, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { useAuthProvider } from "src/auth/useAuthProvider";
 
 export function useNavpill() {
+  const { asPath } = useRouter();
   const overlayRef = useRef<HTMLDivElement>() as RefObject<HTMLDivElement>;
   const [isExpanded, setExpanded] = useState(false);
   const { account } = useAuthProvider();
+
+  // Close the menu for either navigation events or outside clicks/taps:
+
+  useEffect(() => setExpanded(false), [asPath]);
 
   useOutsideClick({
     ref: overlayRef,
