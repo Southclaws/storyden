a682295ee455a1463bb592c066a3123055d4a135
Barnaby Keene <accounts@southcla.ws>
2023-05-10 13:53:49 +0100

    sexy fuckin title input totally not stolen from substack's editor 😶

--- web/src/screens/compose/ComposeScreen.tsx
+++ web/src/screens/compose/ComposeScreen.tsx
@@ -2,25 +2,18 @@ import {
   Button,
   FormErrorMessage,
   HStack,
-  Heading,
   Input,
   VStack,
 } from "@chakra-ui/react";
+import { FormEvent } from "react";
 import { Controller } from "react-hook-form";
 import { Editor } from "src/components/Editor";
 import { CategorySelect } from "./components/CategorySelect/CategorySelect";
 import { useComposeScreen } from "./useComposeScreen";
 
 export function ComposeScreen() {
-  const {
-    onSubmit,
-    handleSubmit,
-    control,
-    register,
-    isValid,
-    errors,
-    isSubmitting,
-  } = useComposeScreen();
+  const { onSubmit, handleSubmit, control, isValid, errors, isSubmitting } =
+    useComposeScreen();
 
   return (
     <VStack
@@ -29,8 +22,6 @@ export function ComposeScreen() {
       w="full"
       py={5}
     >
-      <Heading>New thread</Heading>
-
       <VStack
         as="form"
         onSubmit={handleSubmit(onSubmit)}
@@ -38,7 +29,58 @@ export function ComposeScreen() {
         w="full"
         gap={2}
       >
-        <Input placeholder="Title" {...register("title")} />
+        <HStack width="full" justifyContent="end">
+          <Controller
+            render={({ field: { onChange, ...field } }) => {
+              function onInput(e: FormEvent<HTMLElement>) {
+                // NOTE: not sure which event type to use here...
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                onChange((e.target as any).textContent);
+              }
+
+              return (
+                <Input
+                  as="span"
+                  contentEditable
+                  variant="unstyled"
+                  fontSize="3xl"
+                  overflowWrap="break-word"
+                  wordBreak="break-word"
+                  fontWeight="semibold"
+                  placeholder="Thread title"
+                  onInput={onInput}
+                  {...field}
+                />
+              );
+            }}
+            control={control}
+            name="title"
+          />
+
+          <HStack>
+            <Controller
+              render={({ field }) => (
+                <>
+                  <CategorySelect {...field} />
+                  <FormErrorMessage>
+                    {errors.category?.message}
+                  </FormErrorMessage>
+                </>
+              )}
+              control={control}
+              name="category"
+            />
+
+            <Button
+              type="submit"
+              isDisabled={!isValid}
+              isLoading={isSubmitting}
+            >
+              Post
+            </Button>
+          </HStack>
+        </HStack>
+
         <FormErrorMessage>{errors.title?.message}</FormErrorMessage>
 
         <Controller
@@ -47,28 +89,23 @@ export function ComposeScreen() {
           name="body"
         />
         <FormErrorMessage>{errors.body?.message}</FormErrorMessage>
-
-        <HStack>
-          <Controller
-            render={({ field }) => (
-              <>
-                <CategorySelect {...field} />
-                <FormErrorMessage>{errors.category?.message}</FormErrorMessage>
-              </>
-            )}
-            control={control}
-            name="category"
-          />
-
-          <Button
-            type="submit" //
-            isDisabled={!isValid}
-            isLoading={isSubmitting}
-          >
-            Post
-          </Button>
-        </HStack>
       </VStack>
+
+      <style jsx global>{`
+        [contenteditable="true"]:empty:before {
+          content: "Thread title...";
+          color: gray;
+        }
+
+        /* prevents the user from being able to make a (visible) newline */
+        [contenteditable="true"] br {
+          display: none;
+        }
+
+        .remirror-editor {
+          box-shadow: none !important;
+        }
+      `}</style>
     </VStack>
   );
 }
