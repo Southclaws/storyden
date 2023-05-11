b56994a085efb9d80a9427fa18b16eed1bf9d151
Barnaby Keene <accounts@southcla.ws>
2023-05-11 11:33:16 +0100

    add updated at to byline

---
+++ web/src/utils/date.ts
@@ -0,0 +1,31 @@
+const formatDistanceLocale = {
+  lessThanXSeconds: "{{count}}s",
+  xSeconds: "{{count}}s",
+  halfAMinute: "30s",
+  lessThanXMinutes: "{{count}}m",
+  xMinutes: "{{count}}m",
+  aboutXHours: "{{count}}h",
+  xHours: "{{count}}h",
+  xDays: "{{count}}d",
+  aboutXWeeks: "{{count}}w",
+  xWeeks: "{{count}}w",
+  aboutXMonths: "{{count}}m",
+  xMonths: "{{count}}m",
+  aboutXYears: "{{count}}y",
+  xYears: "{{count}}y",
+  overXYears: "{{count}}y",
+  almostXYears: "{{count}}y",
+};
+
+export const formatDistance = (
+  token: keyof typeof formatDistanceLocale,
+  count: string
+) => {
+  const result = formatDistanceLocale[token].replace("{{count}}", count);
+
+  return result;
+};
+
+export const formatDistanceDefaults = {
+  locale: { formatDistance },
+};
