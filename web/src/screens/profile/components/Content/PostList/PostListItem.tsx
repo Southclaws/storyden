1a727a99f569eeeedceab92f52ad35abf493d2af
Barnaby Keene <accounts@southcla.ws>
2023-05-21 16:08:45 +0100

    customise post list item a bit to match context (no need for the profile reference when it's in the context of a profile already)

---
+++ web/src/screens/profile/components/Content/PostList/PostListItem.tsx
@@ -0,0 +1,24 @@
+import { Flex } from "@chakra-ui/react";
+import { PostProps } from "src/api/openapi/schemas";
+import { Markdown } from "src/components/Markdown";
+
+import { Byline } from "src/screens/thread/components/Byline";
+import { PostMenu } from "src/screens/thread/components/PostMenu/PostMenu";
+import { ReactList } from "src/screens/thread/components/ReactList/ReactList";
+
+export function PostListItem(props: PostProps) {
+  return (
+    <Flex id={props.id} flexDir="column" gap={2}>
+      <Markdown>{props.body}</Markdown>
+      <ReactList {...props} />
+
+      <Byline
+        href={`#${props.id}`}
+        author={props.author.handle}
+        time={new Date(props.createdAt)}
+        updated={new Date(props.updatedAt)}
+        more={<PostMenu {...props} />}
+      />
+    </Flex>
+  );
+}
