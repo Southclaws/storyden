13092d40d153e0c0fc789205cb451c2da79df1f5
Barnaby Keene <accounts@southcla.ws>
2023-05-27 11:57:08 +0100

    add missing key prop to thread list

--- web/src/screens/home/components/ThreadList.tsx
+++ web/src/screens/home/components/ThreadList.tsx
@@ -1,6 +1,7 @@
 import { Divider, List } from "@chakra-ui/react";
 import { ThreadReference } from "src/api/openapi/schemas";
 import { ThreadListItem } from "./ThreadListItem";
+import { Fragment } from "react";
 
 type Props = { threads: ThreadReference[] };
 
@@ -8,10 +9,10 @@ export function ThreadList(props: Props) {
   return (
     <List width="full" display="flex" flexDirection="column">
       {props.threads.map((t) => (
-        <>
+        <Fragment key={t.id}>
           <Divider />
           <ThreadListItem key={t.id} thread={t} />
-        </>
+        </Fragment>
       ))}
     </List>
   );
