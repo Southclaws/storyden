a28f4ab109fea21993622b882eed77de29f4559d
Barnaby Keene <accounts@southcla.ws>
2023-06-04 22:01:21 +0100

    remove unnecessary dynamic import in compose screen

--- web/src/app/(compose)/new/page.tsx
+++ web/src/app/(compose)/new/page.tsx
@@ -1,27 +1,11 @@
 "use client";
 
-import { z } from "zod";
-import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
 
-// NOTE: Render client side, probably unnecessary but quick fix to react errors.
-const ComposeScreen = dynamic(
-  () =>
-    import("../../../screens/compose/ComposeScreen").then(
-      (mod) => mod.ComposeScreen
-    ),
-  {
-    ssr: false,
-  }
-);
-
-const QuerySchema = z.object({
-  id: z.string().optional(),
-});
+import { ComposeScreen } from "src/screens/compose/ComposeScreen";
 
 export default function Page() {
   const params = useSearchParams();
-  const { id } = QuerySchema.parse(params);
 
-  return <ComposeScreen editing={id} />;
+  return <ComposeScreen editing={params.get("id") ?? undefined} />;
 }
