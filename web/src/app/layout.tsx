cd72f76341df71809a13acf8956309056d3a71be
Barnaby Keene <accounts@southcla.ws>
2023-06-04 22:01:05 +0100

    fix a tiny console warning related to browser extensions causing slight differences in the body attributes

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from "react";
+
 import { Providers } from "./providers";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <Providers>{children}</Providers>
       </body>
     </html>
