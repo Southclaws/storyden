73200ffd50a873f498031238327a62ff352c91d5
Barnaby Keene <accounts@southcla.ws>
2023-07-05 21:35:14 +0100

    fix weird slate bug with images duplicating

--- web/src/components/ContentComposer/ContentComposer.tsx
+++ web/src/components/ContentComposer/ContentComposer.tsx
@@ -1,5 +1,6 @@
 import { Box, BoxProps } from "@chakra-ui/react";
 import { PropsWithChildren, useCallback } from "react";
+import { Transforms } from "slate";
 import { Editable, Slate } from "slate-react";
 
 import { FileDrop } from "./components/FileDrop/FileDrop";
@@ -26,6 +27,25 @@ export function ContentComposer({
           <Editable
             renderLeaf={renderLeaf}
             renderElement={renderElement}
+            onKeyDown={(event: React.KeyboardEvent<HTMLElement>) => {
+              // NOTE: this hook prevents Slate from duplicating the previous
+              // node (which results in images being duplicated.)
+
+              if (event.key === "Enter") {
+                event.preventDefault();
+
+                Transforms.insertNodes(editor, [
+                  {
+                    type: "paragraph",
+                    children: [
+                      {
+                        text: "",
+                      },
+                    ],
+                  },
+                ]);
+              }
+            }}
             readOnly={disabled}
             placeholder="Write your heart out..."
             style={{
