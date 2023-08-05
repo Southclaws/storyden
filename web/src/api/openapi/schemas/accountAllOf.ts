ef0991b1293978c99364ecb27df5b79e08961165
Barnaby Keene <accounts@southcla.ws>
2023-08-05 18:11:55 +0100

    fix schema issue with name being optional

--- web/src/api/openapi/schemas/accountAllOf.ts
+++ web/src/api/openapi/schemas/accountAllOf.ts
@@ -11,7 +11,7 @@ import type { TagList } from "./tagList";
 
 export type AccountAllOf = {
   handle: AccountHandle;
-  name?: AccountName;
+  name: AccountName;
   bio?: string;
   interests?: TagList;
 };
