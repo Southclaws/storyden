9c75018c478586a891f09bab28cc5898300f627a
Barnaby Keene <accounts@southcla.ws>
2023-08-12 12:31:30 +0100

    add ModalDrawer component attempting to mix Vaul and Modal into a single view disclosure component

---
+++ web/src/components/Modaldrawer/Modaldrawer.tsx
@@ -0,0 +1,78 @@
+import { Box, CloseButton, UseDisclosureProps, VStack } from "@chakra-ui/react";
+import { PropsWithChildren } from "react";
+import { Drawer } from "vaul";
+
+type Props = UseDisclosureProps;
+
+export function ModalDrawer({ children, ...props }: PropsWithChildren<Props>) {
+  const onOpenChange = (open: boolean) => {
+    if (open) props.onOpen?.();
+    else props.onClose?.();
+  };
+
+  return (
+    <>
+      <Drawer.Root
+        open={props.isOpen}
+        onOpenChange={onOpenChange}
+        shouldScaleBackground
+      >
+        <Drawer.Portal>
+          <Drawer.Overlay className="modaldrawer__overlay" />
+          <Drawer.Content className="modaldrawer__content">
+            <VStack
+              height={{ base: "full", md: "unset" }}
+              borderTopRadius="1em"
+              borderBottomRadius={{ base: "0", md: "1em" }}
+              bgColor="gray.100"
+              alignItems="end"
+              p={3}
+              px={3}
+            >
+              <CloseButton onClick={props.onClose} />
+              <Box w="full" px={3} pb={3}>
+                {children}
+              </Box>
+            </VStack>
+          </Drawer.Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+
+      <style jsx global>{`
+        .modaldrawer__overlay {
+          position: fixed;
+          inset: 0;
+          background-color: var(--chakra-colors-blackAlpha-600);
+        }
+
+        /* Modal mode - on desktop screens */
+        @media screen and (min-width: 48em) {
+          .modaldrawer__content {
+            height: 100%;
+            width: 100%;
+            top: 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            position: fixed;
+          }
+        }
+
+        /* Drawer mode - on mobile screens */
+        @media screen and (max-width: 48em) {
+          .modaldrawer__content {
+            height: 100%;
+            width: 100%;
+            top: 0;
+            display: flex;
+            flex-direction: column;
+            position: fixed;
+            margin-top: 3rem;
+            max-height: 96%;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
