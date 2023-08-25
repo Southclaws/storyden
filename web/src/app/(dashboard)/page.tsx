ac92c38d8233b257fe47e073787b7539ada29664
Barnaby Keene <accounts@southcla.ws>
2023-08-25 18:41:57 +0100

    add metadata and icon

--- web/src/app/(dashboard)/page.tsx
+++ web/src/app/(dashboard)/page.tsx
@@ -1,10 +1,17 @@
+import type { Metadata } from "next";
+
 import { HomeScreen } from "src/screens/home/HomeScreen";
+import { getInfo } from "src/utils/info";
 
 export default async function Page() {
   return <HomeScreen />;
 }
 
-export const metadata = {
-  title: "Storyden",
-  description: "A forum for the modern age.",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const info = await getInfo();
+
+  return {
+    title: info.title,
+    description: info.description,
+  };
+}
