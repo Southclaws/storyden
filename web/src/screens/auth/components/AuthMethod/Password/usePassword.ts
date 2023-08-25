063a893d02dd90f5713005b652706c5389aa3397
Barnaby Keene <accounts@southcla.ws>
2023-08-25 13:57:34 +0100

    fix non-immediate revalidation for auth

--- web/src/screens/auth/components/AuthMethod/Password/usePassword.ts
+++ web/src/screens/auth/components/AuthMethod/Password/usePassword.ts
@@ -2,9 +2,11 @@ import { useToast } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { useAccountGet } from "src/api/openapi/accounts";
 import { authPasswordSignin, authPasswordSignup } from "src/api/openapi/auth";
 import { APIError } from "src/api/openapi/schemas";
-import * as z from "zod";
 
 export const FormSchema = z.object({
   identifier: z.string(),
@@ -22,11 +24,13 @@ export function usePassword() {
   });
   const toast = useToast();
   const { push } = useRouter();
+  const { mutate } = useAccountGet();
 
   async function signin(payload: Form) {
     await authPasswordSignin(payload)
       .then(() => {
         push("/");
+        mutate();
       })
       .catch((e: APIError) =>
         toast({
@@ -41,6 +45,7 @@ export function usePassword() {
     await authPasswordSignup(payload)
       .then(() => {
         push("/");
+        mutate();
       })
       .catch((e: APIError) =>
         toast({
