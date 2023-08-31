325d76d0a8d87867506f543a897cbb05d553ddbe
Barnaby Keene <accounts@southcla.ws>
2023-08-31 16:08:56 +0100

    add a global SSR style sheet that includes the accent colout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -7,6 +7,16 @@ import { Providers } from "./providers";
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
+      <head>
+        {/*
+            NOTE: This stylesheet is fully server-side rendered but it's not
+            static because it uses data from the API to be generated. But we
+            don't want this to require client-side render or CSS-in-JS.
+        */}
+        {/* eslint-disable-next-line @next/next/no-css-tags */}
+        <link rel="stylesheet" href="/theme.css" />
+      </head>
+
       <body suppressHydrationWarning={true}>
         <Providers>{children}</Providers>
       </body>
