8c34cec712d37be7069f3d639dc74576bce60973
Barnaby Keene <accounts@southcla.ws>
2023-09-02 15:21:25 +0100

    spice up the error banner a bit

--- web/src/components/ErrorBanner.tsx
+++ web/src/components/ErrorBanner.tsx
@@ -1,18 +1,20 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
 import { CreateToastFnReturn } from "@chakra-ui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
-import { APIError } from "src/api/openapi/schemas";
+
+import { APIError, APIErrorMetadata } from "src/api/openapi/schemas";
 
 type Props = {
   message?: string | undefined;
+  metadata?: APIErrorMetadata | undefined;
 };
 
-export default function ErrorBanner({ message, ...rest }: Props) {
+export default function ErrorBanner({ message, metadata }: Props) {
   return (
     <Flex width="full" justifyContent="center">
       <Flex flexDir="column" gap={2} bgColor="red.50" borderRadius="xl" p={4}>
         <Flex gap={2} alignItems="center">
-          <ExclamationTriangleIcon width={4} height={4} />
+          <ExclamationTriangleIcon width={32} height={32} />
           <Heading fontSize="md" my="0">
             that&apos;s a yikes from me
           </Heading>
@@ -22,14 +24,16 @@ export default function ErrorBanner({ message, ...rest }: Props) {
           <Text>{message ?? "something bad happened :("}</Text>
         </Box>
 
-        <Text fontSize="sm">
-          <details>
-            <summary>
-              <em>the code mumbo jumbo for nerds</em>
-            </summary>
-            <pre>{JSON.stringify(rest, null, 2)}</pre>
-          </details>
-        </Text>
+        {metadata && (
+          <Text fontSize="sm">
+            <details>
+              <summary>
+                <em>the code mumbo jumbo for nerds</em>
+              </summary>
+              <pre>{JSON.stringify(metadata, null, 2)}</pre>
+            </details>
+          </Text>
+        )}
       </Flex>
     </Flex>
   );
