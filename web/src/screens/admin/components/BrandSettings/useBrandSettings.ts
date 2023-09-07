fa1ff21957aeca0cb1880f4b7aed584a3adec45d
Barnaby Keene <accounts@southcla.ws>
2023-09-07 22:58:55 +0100

    add an icon editor and upload

--- web/src/screens/admin/components/BrandSettings/useBrandSettings.ts
+++ web/src/screens/admin/components/BrandSettings/useBrandSettings.ts
@@ -1,11 +1,12 @@
 import { useToast } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { mutate } from "swr";
 import { z } from "zod";
 
 import { adminSettingsUpdate } from "src/api/openapi/admin";
-import { getGetInfoKey } from "src/api/openapi/misc";
+import { getGetInfoKey, iconUpload } from "src/api/openapi/misc";
 import { Info } from "src/api/openapi/schemas";
 import { getColourVariants } from "src/utils/colour";
 
@@ -28,6 +29,19 @@ export function useBrandSettings(props: Props) {
       accentColour: props.accent_colour,
     },
   });
+  const [currentIcon, setCurrentIcon] = useState<File | undefined>(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const icon = await fetch(`/api/v1/info/icon/512x512`);
+
+      const blob = await icon.blob();
+
+      const file = new File([blob], "icon-512x512");
+
+      setCurrentIcon(file);
+    })();
+  }, []);
 
   const updateColour = (colour: string) => {
     try {
@@ -53,6 +67,12 @@ export function useBrandSettings(props: Props) {
     toast({ title: "Settings updated!" });
   });
 
+  const onSaveIcon = async (file: File) => {
+    await iconUpload(file);
+    mutate(getGetInfoKey());
+    toast({ title: "Icon updated!" });
+  };
+
   const onColourChangePreview = (colour: string) => {
     updateColour(colour);
   };
@@ -61,6 +81,8 @@ export function useBrandSettings(props: Props) {
     register: form.register,
     control: form.control,
     onSubmit: onSubmit,
+    currentIcon,
+    onSaveIcon,
     onColourChangePreview,
   };
 }
