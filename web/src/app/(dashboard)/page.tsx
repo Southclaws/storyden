1d7c119a99471d9a2dc369f9c532127c9a394559
Barnaby Keene <accounts@southcla.ws>
2023-09-10 19:23:12 +0100

    tidy up some metadata stuff: expose metadata for all pages, add missing maskable icons explicitly to manifest, use a hex colour for pwa stuff

--- web/src/app/(dashboard)/page.tsx
+++ web/src/app/(dashboard)/page.tsx
@@ -1,17 +1,5 @@
-import type { Metadata } from "next";
-
 import { HomeScreen } from "src/screens/home/HomeScreen";
-import { getInfo } from "src/utils/info";
 
 export default async function Page() {
   return <HomeScreen />;
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  const info = await getInfo();
-
-  return {
-    title: info.title,
-    description: info.description,
-  };
 }
--- web/src/app/manifest.json/route.ts
+++ web/src/app/manifest.json/route.ts
@@ -1,12 +1,15 @@
 import { Manifest } from "next/dist/lib/metadata/types/manifest-types";
 import { NextResponse } from "next/server";
 
+import { getColourAsHex } from "src/utils/colour";
 import { getInfo } from "src/utils/info";
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
   const info = await getInfo();
+
+  const backgroundColour = getColourAsHex(info.accent_colour);
 
   const manifest: Manifest = {
     id: "/",
@@ -18,7 +21,9 @@ export async function GET() {
     // TODO: figure out a good choice for this.
     theme_color: "white",
     // TODO: make sure this makes sense.
-    background_color: info.accent_colour,
+    background_color: backgroundColour,
+
+    // prettier-ignore
     icons: [
       { src: "/api/v1/info/icon/512x512", sizes: "512x512", type: "image/png" },
       { src: "/api/v1/info/icon/32x32", sizes: "32x32", type: "image/png" },
@@ -26,6 +31,12 @@ export async function GET() {
       { src: "/api/v1/info/icon/120x120", sizes: "120x120", type: "image/png" },
       { src: "/api/v1/info/icon/167x167", sizes: "167x167", type: "image/png" },
       { src: "/api/v1/info/icon/152x152", sizes: "152x152", type: "image/png" },
+      { src: "/api/v1/info/icon/512x512", sizes: "512x512", type: "image/png", purpose: "maskable" },
+      { src: "/api/v1/info/icon/32x32", sizes: "32x32", type: "image/png", purpose: "maskable" },
+      { src: "/api/v1/info/icon/180x180", sizes: "180x180", type: "image/png", purpose: "maskable" },
+      { src: "/api/v1/info/icon/120x120", sizes: "120x120", type: "image/png", purpose: "maskable" },
+      { src: "/api/v1/info/icon/167x167", sizes: "167x167", type: "image/png", purpose: "maskable" },
+      { src: "/api/v1/info/icon/152x152", sizes: "152x152", type: "image/png", purpose: "maskable" },
     ],
   };
 
