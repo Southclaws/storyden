dd652037a473b2acfd7da6d58a11eea4669f59da
Barnaby Keene <accounts@southcla.ws>
2023-09-30 21:55:26 +0100

    abstract feed item styles

---
+++ web/src/components/feed/common/FeedItem/FeedItem.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+
+import { styled } from "@/styled-system/jsx";
+
+export function FeedItem({ children }: PropsWithChildren) {
+  return (
+    <styled.article
+      display="flex"
+      flexDir="column"
+      width="full"
+      p={3}
+      gap={2}
+      boxShadow="6px 6px 16px 0px rgba(0, 0, 0, 0.03)"
+      borderRadius="1rem"
+    >
+      {children}
+    </styled.article>
+  );
+}
