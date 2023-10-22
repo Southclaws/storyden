20117b7ba95c2bc5e97c1fd9b45187c11fd43f33
Barnaby Keene <accounts@southcla.ws>
2023-10-22 21:22:25 +0100

    add white and black alpha ramps to panda theme

--- web/panda.config.ts
+++ web/panda.config.ts
@@ -14,7 +14,36 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          whiteAlpha: {
+            50: { value: "rgba(255, 255, 255, 0.04)" },
+            100: { value: "rgba(255, 255, 255, 0.06)" },
+            200: { value: "rgba(255, 255, 255, 0.08)" },
+            300: { value: "rgba(255, 255, 255, 0.16)" },
+            400: { value: "rgba(255, 255, 255, 0.24)" },
+            500: { value: "rgba(255, 255, 255, 0.36)" },
+            600: { value: "rgba(255, 255, 255, 0.48)" },
+            700: { value: "rgba(255, 255, 255, 0.64)" },
+            800: { value: "rgba(255, 255, 255, 0.80)" },
+            900: { value: "rgba(255, 255, 255, 0.92)" },
+          },
+          blackAlpha: {
+            50: { value: "rgba(0, 0, 0, 0.04)" },
+            100: { value: "rgba(0, 0, 0, 0.06)" },
+            200: { value: "rgba(0, 0, 0, 0.08)" },
+            300: { value: "rgba(0, 0, 0, 0.16)" },
+            400: { value: "rgba(0, 0, 0, 0.24)" },
+            500: { value: "rgba(0, 0, 0, 0.36)" },
+            600: { value: "rgba(0, 0, 0, 0.48)" },
+            700: { value: "rgba(0, 0, 0, 0.64)" },
+            800: { value: "rgba(0, 0, 0, 0.80)" },
+            900: { value: "rgba(0, 0, 0, 0.92)" },
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
