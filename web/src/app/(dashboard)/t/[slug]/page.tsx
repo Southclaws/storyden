7e6626636d25f51d1da92c020cf49831c70e9da3
Barnaby Keene <accounts@southcla.ws>
2023-10-26 18:15:57 +0100

    add metadata and opengraph image to thread page route handler

--- web/src/app/(dashboard)/t/[slug]/page.tsx
+++ web/src/app/(dashboard)/t/[slug]/page.tsx
@@ -1,5 +1,29 @@
+import { server } from "src/api/client";
+import { ThreadGetResponse } from "src/api/openapi/schemas";
 import { ThreadScreen } from "src/screens/thread/ThreadScreen";
+import { getInfo } from "src/utils/info";
 
-export default function Page() {
-  return <ThreadScreen />;
+export type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function Page(props: Props) {
+  return <ThreadScreen slug={props.params.slug} />;
+}
+
+export async function generateMetadata({ params }: Props) {
+  const info = await getInfo();
+  const data = await server<ThreadGetResponse>({
+    url: `/v1/threads/${params.slug}`,
+    params: {
+      slug: [params.slug],
+    },
+  });
+
+  return {
+    title: `${data.title} | ${info.title}`,
+    description: data.short,
+  };
 }
