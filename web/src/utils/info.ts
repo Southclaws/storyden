67588451192483cd1a5fb977afcac837d43e4a01
Barnaby Keene <accounts@southcla.ws>
2023-10-26 18:16:38 +0100

    refactor getInfo to use server API client helper

--- web/src/utils/info.ts
+++ web/src/utils/info.ts
@@ -1,21 +1,11 @@
+import { server } from "src/api/client";
 import { GetInfoOKResponse } from "src/api/openapi/schemas";
-import { API_ADDRESS } from "src/config";
 
 import { FALLBACK_COLOUR } from "./colour";
 
 export async function getInfo(): Promise<GetInfoOKResponse> {
   try {
-    const res = await fetch(`${API_ADDRESS}/api/v1/info`);
-
-    if (!res.ok) {
-      throw new Error(
-        `failed to fetch API info endpoint: ${res.status} ${res.statusText}`,
-      );
-    }
-
-    const info = (await res.json()) as GetInfoOKResponse;
-
-    return info;
+    return await server<GetInfoOKResponse>({ url: "/v1/info" });
   } catch (e) {
     console.error(e);
     return {
