3620e85e787fc35e184a24918b1df32cb0cadc47
Barnaby Keene <accounts@southcla.ws>
2023-11-02 20:01:57 +0000

    force ui test page to be dynamic

--- web/src/app/(bare)/xdev/page.tsx
+++ web/src/app/(bare)/xdev/page.tsx
@@ -3,6 +3,8 @@ import { Palette } from "src/screens/xdev/xdev";
 import { getColourVariants } from "src/utils/colour";
 import { getInfo } from "src/utils/info";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const theme = await fetch(`${WEB_ADDRESS}/theme.css`);
   const info = await getInfo();
