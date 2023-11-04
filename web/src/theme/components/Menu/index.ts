3345a93524f5067282771452b18a683033367f8f
Barnaby Keene <accounts@southcla.ws>
2023-11-04 11:25:35 +0000

    fix wrong styled-system import

--- web/src/theme/components/Menu/index.ts
+++ web/src/theme/components/Menu/index.ts
@@ -1,8 +1,8 @@
 import * as Ark from "@ark-ui/react/menu";
-import { styled } from "styled-system/jsx";
 
 import { createStyleContext } from "src/theme/create-style-context";
 
+import { styled } from "@/styled-system/jsx";
 import { type MenuVariantProps, menu } from "@/styled-system/recipes";
 
 const { withProvider, withContext } = createStyleContext(menu);
