2ce0577242839e29e227e2af64c5e1319b6538f6
southclaws <south@cla.ws>
2023-12-26 17:12:45 +0000

    better name for link card

--- web/src/components/directory/links/LinkCardList.tsx
+++ web/src/components/directory/links/LinkCardList.tsx
@@ -1,19 +1,19 @@
 import { LinkListResult } from "src/api/openapi/schemas";
-import { LinkRef } from "src/components/directory/links/LinkRef";
+import { LinkCard } from "src/components/directory/links/LinkCard";
 import { Empty } from "src/components/feed/common/PostRef/Empty";
 
 import { styled } from "@/styled-system/jsx";
 
-export function LinkRefList(props: { result: LinkListResult }) {
+export function LinkCardList(props: { result: LinkListResult }) {
   if (props.result.links.length === 0) {
     return <Empty>no links were found</Empty>;
   }
 
   return (
-    <styled.ol display="flex" flexDir="column" gap="4">
+    <styled.ol w="full" display="flex" flexDir="column" gap="4">
       {props.result.links.map((v) => (
         <styled.li key={v.url}>
-          <LinkRef {...v} />
+          <LinkCard {...v} />
         </styled.li>
       ))}
     </styled.ol>
