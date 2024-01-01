c9fad2c8ff5aece05bb629713ae2d6f07c2e69bb
southclaws <south@cla.ws>
2024-01-01 00:42:09 +0000

    allow hiding of sidebar

--- web/src/components/site/Navigation/Top/Top.tsx
+++ web/src/components/site/Navigation/Top/Top.tsx
@@ -1,3 +1,6 @@
+import { Sidebar } from "src/components/graphics/Sidebar/Sidebar";
+import { Button } from "src/theme/components/Button";
+
 import styles from "../navigation.module.css";
 
 import { ComposeAction } from "../Anchors/Compose";
@@ -9,7 +12,12 @@ import { cx } from "@/styled-system/css";
 import { HStack } from "@/styled-system/jsx";
 import { FrostedGlass } from "@/styled-system/patterns";
 
-export function Top() {
+type Props = {
+  onToggleSidebar: (t: boolean) => void;
+  sidebarState: boolean;
+};
+
+export function Top({ onToggleSidebar, sidebarState }: Props) {
   const { title } = useNavigation();
 
   return (
@@ -19,15 +27,23 @@ export function Top() {
       alignItems="center"
       px="4"
     >
-      <HStack className={styles["topbar-left"]} justify="space-between">
-        <Title>{title}</Title>
-        <ComposeAction>new post</ComposeAction>
+      <HStack className={styles["topbar-left"]}>
+        {/* TODO: Action? */}
+        <Button
+          size="sm"
+          kind="ghost"
+          onClick={() => onToggleSidebar(!sidebarState)}
+        >
+          <Sidebar open={sidebarState} />
+        </Button>
+        <ComposeAction>new</ComposeAction>
       </HStack>
 
-      {/* TODO: Semantic search */}
-      {/* <HStack className={styles["topbar-middle"]}>
-        <Input placeholder="Search content, knowledgebase and links" />
-      </HStack> */}
+      <HStack className={styles["topbar-middle"]} justify="center">
+        {/* TODO: Semantic search */}
+        {/* <Input placeholder="Search content, knowledgebase and links" /> */}
+        <Title>{title}</Title>
+      </HStack>
 
       <HStack className={styles["topbar-right"]}>
         <Toolbar />
