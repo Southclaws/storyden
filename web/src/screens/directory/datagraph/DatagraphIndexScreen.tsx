eb21772ea8bced825a1c5954d13015fd34df9266
southclaws <south@cla.ws>
2024-01-07 12:27:02 +0000

    add links to directory home

--- web/src/screens/directory/datagraph/DatagraphIndexScreen.tsx
+++ web/src/screens/directory/datagraph/DatagraphIndexScreen.tsx
@@ -2,6 +2,8 @@
 
 import { ClusterList } from "src/components/directory/datagraph/ClusterList";
 import { ItemGrid } from "src/components/directory/datagraph/ItemGrid";
+import { LinkCardList } from "src/components/directory/links/LinkCardList";
+import { Empty } from "src/components/feed/common/PostRef/Empty";
 import { Unready } from "src/components/site/Unready";
 import { Heading1, Heading2 } from "src/theme/components/Heading/Index";
 
@@ -10,11 +12,15 @@ import { VStack } from "@/styled-system/jsx";
 import { Props, useDatagraphIndexScreen } from "./useDatagraphIndexScreen";
 
 export function Client(props: Props) {
-  const { ready, data, error } = useDatagraphIndexScreen(props);
+  const { ready, data, empty, error } = useDatagraphIndexScreen(props);
 
   if (!ready) return <Unready {...error} />;
 
-  const { items, clusters } = data;
+  const { items, clusters, links } = data;
+
+  if (empty) {
+    return <Empty>This community knowledgebase is empty.</Empty>;
+  }
 
   return (
     <VStack w="full" alignItems="start">
@@ -22,10 +28,19 @@ export function Client(props: Props) {
 
       <p>You can browse the community&apos;s knowledgebase here.</p>
 
-      <VStack w="full" alignItems="start">
-        <Heading2>New</Heading2>
-        <ItemGrid directoryPath={[]} {...items.data} />
-      </VStack>
+      {items.data.items.length > 0 && (
+        <VStack w="full" alignItems="start">
+          <Heading2>New items</Heading2>
+          <ItemGrid directoryPath={[]} {...items.data} />
+        </VStack>
+      )}
+
+      {links.data.results > 0 && (
+        <VStack w="full" alignItems="start">
+          <Heading2>New links</Heading2>
+          <LinkCardList links={links.data} show={3} />
+        </VStack>
+      )}
 
       <VStack w="full" alignItems="start">
         <Heading2>Clusters</Heading2>
