e0db578ea1f485a90553b4c5987bcc5ac288e028
southclaws <south@cla.ws>
2024-01-18 18:34:21 +0000

    regenerate styles

--- web/styled-system/recipes/index.d.ts
+++ web/styled-system/recipes/index.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export * from './admonition';
 export * from './input';
-export * from './heading-input';
+export * from './title-input';
 export * from './heading';
 export * from './button';
 export * from './link';
@@ -10,3 +10,4 @@ export * from './tabs';
 export * from './checkbox';
 export * from './popover';
 export * from './skeleton';
+export * from './heading-input';
