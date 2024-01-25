c688e9f3b89b4e631284c1a4861020cace1a894c
southclaws <south@cla.ws>
2024-01-25 21:43:56 +0000

    rebuild panda

--- web/styled-system/recipes/index.d.ts
+++ web/styled-system/recipes/index.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export * from './admonition';
 export * from './input';
-export * from './title-input';
+export * from './heading-input';
 export * from './heading';
 export * from './button';
 export * from './link';
@@ -10,5 +10,4 @@ export * from './tabs';
 export * from './checkbox';
 export * from './popover';
 export * from './skeleton';
-export * from './heading-input';
 export * from './card';
