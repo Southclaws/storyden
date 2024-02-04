3ac3001ce2f2aaa0cc0a41118be62eeb1c86f174
southclaws <south@cla.ws>
2024-02-04 11:09:27 +0100

    refactor collection menu to use onSelect and correctly mutate thread list when used

--- web/src/components/content/CollectionMenu/useCollectionMenu.ts
+++ web/src/components/content/CollectionMenu/useCollectionMenu.ts
@@ -1,26 +1,58 @@
+import { contains, map } from "lodash/fp";
 import { KeyboardEvent, useState } from "react";
 
-import { useCollectionList } from "src/api/openapi/collections";
-import { ThreadReference } from "src/api/openapi/schemas";
+import {
+  collectionAddPost,
+  collectionRemovePost,
+  useCollectionList,
+} from "src/api/openapi/collections";
+import {
+  Collection,
+  CollectionList,
+  ThreadReference,
+} from "src/api/openapi/schemas";
 import { useSession } from "src/auth";
+import { useFeedState } from "src/components/feed/useFeedState";
 import { useDisclosure } from "src/utils/useDisclosure";
 
 export type Props = {
   thread: ThreadReference;
 };
 
+export type CollectionWithHasPost = Collection & {
+  hasPost: boolean;
+};
+
+const hydrateState = (
+  collections: CollectionList,
+  threadCollections: CollectionList,
+): CollectionWithHasPost[] => {
+  const cids = threadCollections.map((v) => v.id);
+
+  const m = map((c: Collection) => ({
+    ...c,
+    hasPost: contains(c.id)(cids),
+  }));
+
+  return m(collections);
+};
+
 export function useCollectionMenu({ thread }: Props) {
   const account = useSession();
-  const { data: collections, error } = useCollectionList();
+  const { data, error } = useCollectionList();
+  const { mutate } = useFeedState();
+
   const [multiSelect, setMultiSelect] = useState(false);
   const [selected, setSelected] = useState(0);
+
+  const collections = hydrateState(data?.collections ?? [], thread.collections);
 
   const handleReset = () => {
     setMultiSelect(false);
     setSelected(0);
   };
 
-  const { onOpenChange, onToggle } = useDisclosure({
+  const { onOpenChange: handleOpenChange, onToggle } = useDisclosure({
     onClose: handleReset,
   });
 
@@ -35,10 +67,11 @@ export function useCollectionMenu({ thread }: Props) {
     thread.collections.filter((c) => c.owner.id === account?.id).length,
   );
 
-  const onKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
     if (e.shiftKey) setMultiSelect(true);
   };
-  const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+
+  const handleKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
     if (!e.shiftKey && multiSelect) {
       setMultiSelect(false);
       if (selected > 0) {
@@ -47,13 +80,33 @@ export function useCollectionMenu({ thread }: Props) {
     }
   };
 
+  const handleSelect = async ({ value: id }: { value: string }) => {
+    switch (id) {
+      case "create-collection":
+        // handled by <Button />
+        return;
+
+      default:
+        if (thread.collections.find((v) => v.id === id)) {
+          await collectionRemovePost(id, thread.id);
+        } else {
+          await collectionAddPost(id, thread.id);
+        }
+
+        await mutate?.();
+    }
+  };
+
   return {
     ready: true as const,
     isAlreadySaved,
-    collections: collections.collections,
+    collections,
     multiSelect,
-    onKeyDown,
-    onKeyUp,
-    onOpenChange,
+    handlers: {
+      handleKeyDown,
+      handleKeyUp,
+      handleOpenChange,
+      handleSelect,
+    },
   };
 }
