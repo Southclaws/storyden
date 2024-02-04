1186fa17b93f0c9e851c27ea6f4521a9fc26fcf8
southclaws <south@cla.ws>
2024-02-04 11:34:43 +0100

    remove reacts, broken

--- web/src/screens/profile/components/PostList/PostListItem.tsx
+++ web/src/screens/profile/components/PostList/PostListItem.tsx
@@ -2,7 +2,6 @@ import { PostProps } from "src/api/openapi/schemas";
 import { Byline } from "src/components/content/Byline";
 import { ContentViewer } from "src/components/content/ContentViewer/ContentViewer";
 import { PostMenu } from "src/components/thread/PostMenu/PostMenu";
-import { ReactList } from "src/components/thread/ReactList/ReactList";
 
 import { Flex } from "@/styled-system/jsx";
 
@@ -10,7 +9,6 @@ export function PostListItem(props: PostProps) {
   return (
     <Flex id={props.id} flexDir="column" gap="2">
       <ContentViewer value={props.body} />
-      <ReactList {...props} />
 
       <Byline
         href={`#${props.id}`}
