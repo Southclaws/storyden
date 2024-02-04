9088f771f93a0a3ffb9d0ec451cf5160ce4d1a0f
southclaws <south@cla.ws>
2024-02-04 11:09:27 +0100

    refactor collection menu to use onSelect and correctly mutate thread list when used

--- web/src/components/content/CollectionMenu/CollectionMenu.tsx
+++ web/src/components/content/CollectionMenu/CollectionMenu.tsx
@@ -1,33 +1,42 @@
 import { Portal } from "@ark-ui/react";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/solid";
 
 import { BookmarkAction } from "src/components/site/Action/Bookmark";
+import { Checkbox } from "src/theme/components/Checkbox";
+import {
+  MenuContent,
+  MenuItem,
+  MenuItemGroup,
+  MenuItemGroupLabel,
+  MenuSeparator,
+} from "src/theme/components/Menu";
 import { Menu, MenuPositioner, MenuTrigger } from "src/theme/components/Menu";
 
+import { CollectionCreateTrigger } from "../CollectionCreate/CollectionCreateTrigger";
+
+import { Center, HStack } from "@/styled-system/jsx";
 import { Box } from "@/styled-system/jsx";
 
-import { CollectionMenuItems } from "./CollectionMenuItems";
 import { Props, useCollectionMenu } from "./useCollectionMenu";
 
 export function CollectionMenu(props: Props) {
-  const {
-    ready,
-    collections,
-    multiSelect,
-    onKeyDown,
-    onKeyUp,
-    onOpenChange,
-    isAlreadySaved,
-  } = useCollectionMenu(props);
+  const { ready, collections, multiSelect, isAlreadySaved, handlers } =
+    useCollectionMenu(props);
 
   if (!ready) return null;
 
   return (
-    <Box onKeyDown={onKeyDown} onKeyUp={onKeyUp} tabIndex={1}>
+    <Box
+      onKeyDown={handlers.handleKeyDown}
+      onKeyUp={handlers.handleKeyUp}
+      tabIndex={1}
+    >
       <Menu
         size="sm"
-        onOpenChange={onOpenChange}
+        onOpenChange={handlers.handleOpenChange}
         closeOnSelect={!multiSelect}
         userSelect="none"
+        onSelect={handlers.handleSelect}
       >
         <MenuTrigger asChild>
           <BookmarkAction bookmarked={isAlreadySaved} />
@@ -35,11 +44,36 @@ export function CollectionMenu(props: Props) {
 
         <Portal>
           <MenuPositioner>
-            <CollectionMenuItems
-              initialCollections={collections}
-              thread={props.thread}
-              multiSelect={multiSelect}
-            />
+            <MenuContent>
+              <MenuItemGroup id="group">
+                <MenuItemGroupLabel htmlFor="group">
+                  Add to collections
+                </MenuItemGroupLabel>
+
+                <MenuSeparator />
+
+                {collections.map((c) => (
+                  <MenuItem id={c.id} key={c.id}>
+                    <HStack>
+                      {multiSelect ? (
+                        <Checkbox checked={c.hasPost} />
+                      ) : (
+                        <Center w="5">
+                          {c.hasPost ? <MinusIcon /> : <PlusIcon />}
+                        </Center>
+                      )}
+                      {c.name}
+                    </HStack>
+                  </MenuItem>
+                ))}
+              </MenuItemGroup>
+
+              <MenuItemGroup id="create">
+                <MenuItem id="create-collection" closeOnSelect={false}>
+                  <CollectionCreateTrigger kind="blank" />
+                </MenuItem>
+              </MenuItemGroup>
+            </MenuContent>
           </MenuPositioner>
         </Portal>
       </Menu>
