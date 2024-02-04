a860c9e2aede790708ba002b189b02a232706269
southclaws <south@cla.ws>
2024-02-04 01:32:45 +0100

    fix icon editor on dark mode

--- web/src/screens/admin/components/BrandSettings/IconEditor/IconEditor.tsx
+++ web/src/screens/admin/components/BrandSettings/IconEditor/IconEditor.tsx
@@ -2,7 +2,7 @@ import AvatarEditor from "react-avatar-editor";
 
 import { Button } from "src/theme/components/Button";
 
-import { HStack, VStack, styled } from "@/styled-system/jsx";
+import { Box, HStack, VStack, styled } from "@/styled-system/jsx";
 
 import { Props, useIconEditor } from "./useIconEditor";
 
@@ -14,57 +14,62 @@ export function IconEditor(props: Props) {
 
   return (
     <VStack alignItems="start">
-      <HStack justify="start" alignItems="end">
-        <AvatarEditor
-          ref={ref}
-          image={file}
-          width={136}
-          height={136}
-          borderRadius={12}
-          style={editorStyle}
-          border={0}
-          color={[255, 255, 255, 1]}
-          scale={1}
-          position={position}
-          onPositionChange={saving ? undefined : setPosition}
-        />
-        <VStack alignItems="start">
-          <HStack>
-            <AvatarEditor
-              image={file}
-              width={32}
-              height={32}
-              borderRadius={6}
-              style={editorStyle}
-              border={0}
-              color={[255, 255, 255, 1]}
-              scale={1}
-              position={position}
-            />
-            <AvatarEditor
-              image={file}
-              width={32}
-              height={32}
-              borderRadius={99}
-              style={editorStyle}
-              border={0}
-              color={[255, 255, 255, 1]}
-              scale={1}
-              position={position}
-            />
-          </HStack>
-
+      <HStack>
+        <Box borderRadius="lg" overflow="hidden">
           <AvatarEditor
+            ref={ref}
             image={file}
-            width={96}
-            height={96}
-            borderRadius={12}
+            width={136}
+            height={136}
             style={editorStyle}
             border={0}
             color={[255, 255, 255, 1]}
             scale={1}
             position={position}
+            onPositionChange={saving ? undefined : setPosition}
           />
+        </Box>
+        <VStack alignItems="start" gap="2" justifyContent="space-between">
+          <HStack>
+            <Box borderRadius="md" overflow="hidden">
+              <AvatarEditor
+                image={file}
+                width={32}
+                height={32}
+                style={editorStyle}
+                border={0}
+                color={[255, 255, 255, 1]}
+                scale={1}
+                position={position}
+              />
+            </Box>
+
+            <Box borderRadius="full" overflow="hidden">
+              <AvatarEditor
+                image={file}
+                width={32}
+                height={32}
+                style={editorStyle}
+                border={0}
+                color={[255, 255, 255, 1]}
+                scale={1}
+                position={position}
+              />
+            </Box>
+          </HStack>
+
+          <Box borderRadius="lg" overflow="hidden">
+            <AvatarEditor
+              image={file}
+              width={96}
+              height={96}
+              style={editorStyle}
+              border={0}
+              color={[255, 255, 255, 1]}
+              scale={1}
+              position={position}
+            />
+          </Box>
         </VStack>
       </HStack>
       <>
