9badd90f34a2f77efa00d8dd4df16627822b0cf6
southclaws <south@cla.ws>
2024-02-27 21:09:30 +0000

    rework links

---
+++ web/src/components/directory/links/LinkIndexView/LinkResultList.tsx
@@ -0,0 +1,19 @@
+import { LinkListResult } from "src/api/openapi/schemas";
+import { Empty } from "src/components/site/Empty";
+
+import { LinkCardRows } from "../LinkCardList";
+
+type Props = {
+  links: LinkListResult;
+  show?: number;
+};
+
+export function LinkResultList({ links, show }: Props) {
+  if (links.links.length === 0) {
+    return <Empty>no links were found</Empty>;
+  }
+
+  const shown = show ? links.links.slice(0, show) : links.links;
+
+  return <LinkCardRows links={shown} />;
+}
