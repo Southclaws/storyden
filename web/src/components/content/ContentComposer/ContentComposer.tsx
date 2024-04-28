36371eac3b0c0a0dfd487d0e6c69d8ddfade2b19
Barnaby <accounts@southcla.ws>
2024-04-28 19:12:26 +0100

    move formatting into a separate hook return and implement text block type selection for paragraphs and headings

--- web/src/components/content/ContentComposer/ContentComposer.tsx
+++ web/src/components/content/ContentComposer/ContentComposer.tsx
@@ -1,12 +1,29 @@
+import { Portal } from "@ark-ui/react";
 import { EditorContent } from "@tiptap/react";
 import {
   BoldIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  Heading5Icon,
+  Heading6Icon,
   ImageIcon,
   ItalicIcon,
   StrikethroughIcon,
+  TextIcon,
 } from "lucide-react";
+import { useRef } from "react";
+import { match } from "ts-pattern";
 
 import { Button } from "src/theme/components/Button";
+import {
+  Menu,
+  MenuContent,
+  MenuItem,
+  MenuPositioner,
+  MenuTrigger,
+} from "src/theme/components/Menu";
 
 import "./styles.css";
 
@@ -18,7 +35,7 @@ import { FloatingMenu } from "./plugins/MenuPlugin";
 import { Props, useContentComposer } from "./useContentComposer";
 
 export function ContentComposer(props: Props) {
-  const { editor, handlers } = useContentComposer(props);
+  const { editor, handlers, format } = useContentComposer(props);
 
   return (
     <LStack
@@ -32,30 +49,97 @@ export function ContentComposer(props: Props) {
     >
       {editor && (
         <FloatingMenu editor={editor}>
+          <Menu
+            size="sm"
+            userSelect="none"
+            onSelect={(d) => format.text.set(d.value as any /* lazy */)}
+          >
+            <MenuTrigger asChild>
+              <Button
+                type="button"
+                size="xs"
+                kind="ghost"
+                title="Change the kind of text"
+              >
+                {match(format.text.active)
+                  .with("p", () => <TextIcon />)
+                  .with("h1", () => <Heading1Icon />)
+                  .with("h2", () => <Heading2Icon />)
+                  .with("h3", () => <Heading3Icon />)
+                  .with("h4", () => <Heading4Icon />)
+                  .otherwise(() => (
+                    <TextIcon />
+                  ))}
+              </Button>
+            </MenuTrigger>
+
+            <Portal>
+              {/* NOTE: Because this is a portal, we need to reference this ID
+              in the FloatingMenu unfocus logic so we don't hide the menu when
+              this menu is opened as it's a portal and not a child element. */}
+              <MenuPositioner>
+                <MenuContent
+                  id="text-block-menu"
+                  backdropBlur="md"
+                  backdropFilter="auto"
+                >
+                  <MenuItem id="p">
+                    <TextIcon />
+                    &nbsp;Paragraph
+                  </MenuItem>
+
+                  <MenuItem id="h1" fontSize="lg" fontWeight="extrabold">
+                    <Heading1Icon />
+                    &nbsp;Heading 1
+                  </MenuItem>
+                  <MenuItem id="h2" fontSize="md" fontWeight="extrabold">
+                    <Heading2Icon />
+                    &nbsp;Heading 2
+                  </MenuItem>
+                  <MenuItem id="h3" fontSize="md" fontWeight="bold">
+                    <Heading3Icon />
+                    &nbsp;Heading 3
+                  </MenuItem>
+                  <MenuItem id="h4" fontSize="md" fontWeight="medium">
+                    <Heading4Icon />
+                    &nbsp;Heading 4
+                  </MenuItem>
+                  <MenuItem id="h5" fontSize="sm" fontWeight="normal">
+                    <Heading5Icon />
+                    &nbsp;Heading 5
+                  </MenuItem>
+                  <MenuItem id="h6" fontSize="sm" fontWeight="light">
+                    <Heading6Icon />
+                    &nbsp;Heading 6
+                  </MenuItem>
+                </MenuContent>
+              </MenuPositioner>
+            </Portal>
+          </Menu>
           <Button
             type="button"
             size="xs"
-            kind="ghost"
+            kind={format.bold.isActive ? "primary" : "ghost"}
             title="Toggle bold text"
-            onClick={handlers.handleBold}
+            onClick={format.bold.toggle}
           >
             <BoldIcon />
           </Button>
           <Button
             type="button"
             size="xs"
-            kind="ghost"
+            kind={format.italic.isActive ? "primary" : "ghost"}
             title="Toggle italic text"
-            onClick={handlers.handleItalic}
+            onClick={format.italic.toggle}
           >
             <ItalicIcon />
           </Button>
           <Button
             type="button"
             size="xs"
-            kind="ghost"
+            kind={format.strike.isActive ? "primary" : "ghost"}
             title="Toggle strikeout text"
-            onClick={handlers.handleStrike}
+            onClick={format.strike.toggle}
           >
             <StrikethroughIcon />
           </Button>
