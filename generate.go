80c37e0869404291d7df63ccc3d57ad3a79e6ade
Barnaby <accounts@southcla.ws>
2024-06-15 19:26:43 +0100

    migrate oapi-codegen to new import path

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
 package storyden
 
 // Generates the OpenAPI stubs for the API server and client.
-//go:generate go run -mod=mod github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 --config api/config.yaml api/openapi.yaml
+//go:generate go run -mod=mod github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.3.0 --config api/config.yaml api/openapi.yaml
