2d553f6426b07cd3f638d4ab40f7a5466fcd18ac
Barnaby <accounts@southcla.ws>
2024-06-23 12:53:54 +0100

    remove some unused imports

--- web/src/components/ui/tree-view.tsx
+++ web/src/components/ui/tree-view.tsx
@@ -3,7 +3,6 @@ import {
   TreeView as ArkTreeView,
   type TreeViewRootProps,
 } from "@ark-ui/react/tree-view";
-import { CircleDashed, CircleIcon, DotIcon } from "lucide-react";
 import Link from "next/link";
 import { forwardRef } from "react";
 
