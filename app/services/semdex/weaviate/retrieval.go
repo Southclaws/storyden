81603624482d00e4f36a62559455460338cb8d2f
Barnaby <accounts@southcla.ws>
2024-06-30 17:07:06 +0100

    auto clean up invalid objects in weaviate during reindex/sync

--- app/services/semdex/weaviate/retrieval.go
+++ app/services/semdex/weaviate/retrieval.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 
-	"github.com/Southclaws/dt"
 	"github.com/Southclaws/fault"
 	"github.com/Southclaws/fault/fctx"
 	"github.com/Southclaws/storyden/app/resources/datagraph"
@@ -20,9 +19,23 @@ func (o *weaviateSemdexer) GetAll(ctx context.Context) (datagraph.NodeReferenceL
 		return nil, fault.Wrap(err, fctx.With(ctx))
 	}
 
-	refs, err := dt.MapErr(objects, mapWeaviateObject)
-	if err != nil {
-		return nil, err
+	invalid := []*models.Object{}
+	refs := []*datagraph.NodeReference{}
+
+	for _, m := range objects {
+		o, err := mapWeaviateObject(m)
+		if err != nil {
+			invalid = append(invalid, m)
+			continue
+		}
+
+		refs = append(refs, o)
+	}
+
+	for _, m := range invalid {
+		if err := o.wc.Data().Deleter().WithID(m.ID.String()).Do(ctx); err != nil {
+			return nil, fault.Wrap(err, fctx.With(ctx))
+		}
 	}
 
 	return datagraph.NodeReferenceList(refs), nil
