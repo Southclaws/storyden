49a30977eb9f3529b6e31dd04c9e14022746dc76
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-08-11 16:06:51 +0100

    rework thread view

--- web/src/components/thread/PostList.tsx
+++ web/src/components/thread/PostList.tsx
@@ -1,8 +1,9 @@
+import { formatDate, formatDistance, formatDistanceStrict } from "date-fns";
 import { Fragment } from "react";
 
 import { Post } from "src/api/openapi-schema";
 
-import { Divider, styled } from "@/styled-system/jsx";
+import { VStack, styled } from "@/styled-system/jsx";
 
 import { PostView } from "./PostView/PostView";
 
@@ -21,14 +22,49 @@ export function PostListView(props: Props) {
       flexDir="column"
       width="full"
     >
-      {props.posts.map((p) => (
-        <Fragment key={p.id}>
-          <Divider />
-          <styled.li key={p.id} listStyleType="none" m="0">
-            <PostView {...p} />
-          </styled.li>
-        </Fragment>
-      ))}
+      {props.posts.map((p, i) => {
+        const previous = props.posts[i - 1];
+        const start = previous ? new Date(previous.createdAt) : undefined;
+        const end = new Date(p.createdAt);
+
+        return (
+          <Fragment key={p.id}>
+            {start && <IntervalDivider interval={{ start, end }} />}
+
+            <styled.li listStyleType="none" m="0">
+              <PostView {...p} />
+            </styled.li>
+          </Fragment>
+        );
+      })}
     </styled.ol>
+  );
+}
+
+export type IntervalDividerProps = {
+  interval: {
+    start: Date;
+    end: Date;
+  };
+};
+
+export function IntervalDivider({ interval }: IntervalDividerProps) {
+  const difference = interval.end.getTime() - interval.start.getTime();
+
+  if (difference < 8640000) {
+    return null;
+  }
+
+  const startLabel = formatDate(interval.start, "PP");
+  const endLabel = formatDate(interval.end, "PP");
+
+  const title = `${startLabel} - ${formatDistanceStrict(interval.start, interval.end)} - ${endLabel}`;
+
+  return (
+    <VStack w="full" color="fg.subtle" fontSize="xs">
+      <time title={title}>
+        {formatDistance(interval.start, interval.end)} later
+      </time>
+    </VStack>
   );
 }
