f780a7787b9c07b5708920c9c031b4275ed25430
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-09-12 18:52:16 +0100

    don't send notifications for self-mentions

--- app/services/mention/mentioner/mentioner.go
+++ app/services/mention/mentioner/mentioner.go
@@ -5,8 +5,10 @@ import (
 
 	"go.uber.org/zap"
 
+	"github.com/Southclaws/storyden/app/resources/account"
 	"github.com/Southclaws/storyden/app/resources/datagraph"
 	"github.com/Southclaws/storyden/app/resources/mq"
+	"github.com/Southclaws/storyden/app/services/authentication/session"
 	"github.com/Southclaws/storyden/internal/infrastructure/pubsub"
 )
 
@@ -20,7 +22,17 @@ func New(l *zap.Logger, q pubsub.Topic[mq.Mention]) *Mentioner {
 }
 
 func (n *Mentioner) Send(ctx context.Context, source datagraph.Ref, items ...*datagraph.Ref) {
+	sender, err := session.GetAccountID(ctx)
+	if err != nil {
+		n.l.Warn("cannot send notification without source session", zap.Error(err))
+	}
+
 	for _, i := range items {
+		if i.Kind == datagraph.KindProfile && sender == account.AccountID(i.ID) {
+			// Skip self-mentions
+			continue
+		}
+
 		err := n.q.Publish(ctx, mq.Mention{
 			Source: source,
 			Item:   *i,
