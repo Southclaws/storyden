bf4e3eed8a69b034c592ae456dedd559edf640c2
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-09-29 12:51:30 +0100

    remove "nothing inside" and push child items to end of page

--- web/src/screens/directory/datagraph/DatagraphNodeScreen/DatagraphNodeScreen.tsx
+++ web/src/screens/directory/datagraph/DatagraphNodeScreen/DatagraphNodeScreen.tsx
@@ -9,13 +9,11 @@ import { NodeCardRows } from "src/components/directory/datagraph/NodeCardList";
 import { CancelAction } from "src/components/site/Action/Cancel";
 import { EditAction } from "src/components/site/Action/Edit";
 import { SaveAction } from "src/components/site/Action/Save";
-import { Empty } from "src/components/site/Empty";
 
 import { Admonition } from "@/components/ui/admonition";
 import { Heading } from "@/components/ui/heading";
-import { Input } from "@/components/ui/input";
 import * as Popover from "@/components/ui/popover";
-import { HStack, VStack, styled } from "@/styled-system/jsx";
+import { HStack, LStack, VStack, styled } from "@/styled-system/jsx";
 
 import { ContentInput } from "./ContentInput";
 import { TitleInput } from "./TitleInput";
@@ -43,6 +41,7 @@ export function DatagraphNodeScreen(props: Props) {
       display="flex"
       flexDir="column"
       w="full"
+      h="full"
       gap="3"
       alignItems="start"
       onSubmit={handleSubmit}
@@ -58,78 +57,79 @@ export function DatagraphNodeScreen(props: Props) {
       </Admonition>
 
       <FormProvider {...form}>
-        <HStack w="full" justify="space-between">
-          <Breadcrumbs
-            directoryPath={directoryPath}
-            visibility={node.visibility}
-            create={editing ? "edit" : "show"}
-            defaultValue={node.slug}
-            {...form.register("slug")}
-          />
-          {isAllowedToEdit && (
-            <Popover.Root open={isSaving} lazyMount>
-              <Popover.Anchor>
-                <HStack>
-                  {editing ? (
-                    <>
-                      <CancelAction onClick={handleEditMode}>
-                        Cancel
-                      </CancelAction>
-                      <SaveAction type="submit">Save</SaveAction>
-                    </>
-                  ) : (
-                    <>
-                      <EditAction onClick={handleEditMode}>Edit</EditAction>
-                    </>
-                  )}
-                  <DatagraphNodeMenu
-                    node={node}
-                    onVisibilityChange={handleVisibilityChange}
-                    onDelete={handleDelete}
-                  />
-                </HStack>
-              </Popover.Anchor>
+        <LStack h="full">
+          <HStack w="full" justify="space-between">
+            <Breadcrumbs
+              directoryPath={directoryPath}
+              visibility={node.visibility}
+              create={editing ? "edit" : "show"}
+              defaultValue={node.slug}
+              {...form.register("slug")}
+            />
+            {isAllowedToEdit && (
+              <Popover.Root open={isSaving} lazyMount>
+                <Popover.Anchor>
+                  <HStack>
+                    {editing ? (
+                      <>
+                        <CancelAction onClick={handleEditMode}>
+                          Cancel
+                        </CancelAction>
+                        <SaveAction type="submit">Save</SaveAction>
+                      </>
+                    ) : (
+                      <>
+                        <EditAction onClick={handleEditMode}>Edit</EditAction>
+                      </>
+                    )}
+                    <DatagraphNodeMenu
+                      node={node}
+                      onVisibilityChange={handleVisibilityChange}
+                      onDelete={handleDelete}
+                    />
+                  </HStack>
+                </Popover.Anchor>
 
-              <Popover.Positioner>
-                <Popover.Content p="2">Saved!</Popover.Content>
-              </Popover.Positioner>
-            </Popover.Root>
-          )}
-        </HStack>
+                <Popover.Positioner>
+                  <Popover.Content p="2">Saved!</Popover.Content>
+                </Popover.Positioner>
+              </Popover.Root>
+            )}
+          </HStack>
 
-        <VStack w="full" alignItems="start" gap="2">
-          <VStack alignItems="start" w="full" minW="0">
-            <HStack w="full" justify="space-between" alignItems="end">
-              {editing ? (
-                <TitleInput />
-              ) : (
-                <Heading fontSize="heading.2" fontWeight="bold">
-                  {node.name || "(untitled)"}
-                </Heading>
-              )}
-            </HStack>
+          <VStack w="full" alignItems="start" gap="2">
+            <VStack alignItems="start" w="full" minW="0">
+              <HStack w="full" justify="space-between" alignItems="end">
+                {editing ? (
+                  <TitleInput />
+                ) : (
+                  <Heading fontSize="heading.2" fontWeight="bold">
+                    {node.name || "(untitled)"}
+                  </Heading>
+                )}
+              </HStack>
+            </VStack>
           </VStack>
-        </VStack>
 
-        <ContentInput
-          disabled={!editing}
-          onAssetUpload={handleAssetUpload}
-          initialValue={
-            node.content ?? form.formState.defaultValues?.["content"]
-          }
-        />
+          <ContentInput
+            disabled={!editing}
+            onAssetUpload={handleAssetUpload}
+            initialValue={
+              node.content ?? form.formState.defaultValues?.["content"]
+            }
+          />
+        </LStack>
 
-        <VStack alignItems="start" w="full">
-          {node.children.length === 0 && <Empty>Nothing inside</Empty>}
-
-          {node && (node.children.length ?? 0) > 0 && (
+        {node && (node.children.length ?? 0) > 0 && (
+          <LStack alignItems="start" w="full">
+            <Heading>Child pages</Heading>
             <NodeCardRows
               directoryPath={directoryPath}
               context="directory"
               nodes={node.children}
             />
-          )}
-        </VStack>
+          </LStack>
+        )}
       </FormProvider>
     </styled.form>
   );
