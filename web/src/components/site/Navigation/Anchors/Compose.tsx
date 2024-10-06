6946a66befa9108b25573793f6ef26a3d7f6e0e1
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-10-06 10:05:12 +0100

    make new post button clearer

--- web/src/components/site/Navigation/Anchors/Compose.tsx
+++ web/src/components/site/Navigation/Anchors/Compose.tsx
@@ -1,4 +1,4 @@
-import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "lucide-react";
 import { PropsWithChildren } from "react";
 
 import { LinkButton } from "@/components/ui/link-button";
@@ -6,7 +6,7 @@ import { LinkButton } from "@/components/ui/link-button";
 export function ComposeAction(props: PropsWithChildren) {
   return (
     <LinkButton href="/new" variant="ghost" size="sm">
-      <PlusCircleIcon /> {props.children}
+      <PlusIcon /> {props.children}
     </LinkButton>
   );
 }
