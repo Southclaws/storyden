027ba62fef07a6a8736426a09df8e51fb787fa44
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-10-06 09:47:09 +0100

    avoid nextjs bug still unfixed https://github.com/vercel/next.js/pull/49155 by just not rendering navigation layout while on 404 page

--- web/src/app/not-found.tsx
+++ web/src/app/not-found.tsx
@@ -1,10 +1,17 @@
 import { UnreadyBanner } from "src/components/site/Unready";
-import { Default } from "src/layouts/Default";
+
+import { LinkButton } from "@/components/ui/link-button";
+import { VStack } from "@/styled-system/jsx";
 
 export default function Page() {
   return (
-    <Default>
-      <UnreadyBanner error="The link to this page did not lead anywhere." />
-    </Default>
+    <VStack p="4" h="dvh" justify="center">
+      <VStack maxW="sm" minH="60" gap="8">
+        <UnreadyBanner error="The link to this page did not lead anywhere." />
+        <LinkButton variant="subtle" href="/">
+          Go back to the home page
+        </LinkButton>
+      </VStack>
+    </VStack>
   );
 }
