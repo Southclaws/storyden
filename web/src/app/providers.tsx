8bc02c7f026e65a939e696690c43c365b7564c8f
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-10-20 15:50:03 +0100

    ensure swr cache is only initialised after mount

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { useIsClient } from "@uidotdev/usehooks";
 import { PropsWithChildren } from "react";
 import { Toaster } from "sonner";
 import { SWRConfig } from "swr";
 
 import { AuthProvider } from "src/auth/AuthProvider";
 
-import { cacheProvider } from "@/lib/cache/swr-cache";
+import { useCacheProvider } from "@/lib/cache/swr-cache";
 
 export function Providers({ children }: PropsWithChildren) {
-  const isClient = useIsClient();
-
-  const provider = isClient ? cacheProvider() : undefined;
+  const provider = useCacheProvider();
 
   return (
     <AuthProvider>
--- web/src/lib/cache/swr-cache.ts
+++ web/src/lib/cache/swr-cache.ts
@@ -1,44 +1,35 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { Cache, State } from "swr";
 
 const CACHE_KEY_NAME = "swr-cache";
 
-export function cacheProvider<Data = any>(): () => Cache<Data> {
-  const ls = localStorage.getItem(CACHE_KEY_NAME);
-  const parsedCache = ls && JSON.parse(ls);
+type CacheProvider = () => Cache;
 
-  // The cache is an in-memory map for performance.
-  const map = new Map<string, State<Data>>(parsedCache);
+type Data = State<any, any>;
 
-  // When a session ends, the local cache is dumped to local storage. Note that
-  // this is not compatible with multiple browser sessions at the same time, the
-  // most recently closed tab will set the current cache state.
-  window.addEventListener("beforeunload", () => {
-    const appCache = JSON.stringify(Array.from(map.entries()));
-    localStorage.setItem(CACHE_KEY_NAME, appCache);
-  });
+export function useCacheProvider(): CacheProvider {
+  const cache = useRef<Map<string, Data>>(new Map());
 
-  function get(key: string): State<Data> | undefined {
-    return map.get(key) as State<Data>;
-  }
+  useEffect(() => {
+    const appCache = loadCache();
+    if (appCache) {
+      const map = new Map(JSON.parse(appCache) as Iterable<[string, Data]>);
+      map.forEach((value, key) => cache.current.set(key, value));
+    }
 
-  function set(key: string, value: State<Data>) {
-    map.set(key, value);
-  }
+    const saveCache = () => {
+      storeCache(JSON.stringify(Array.from(cache.current.entries())));
+    };
 
-  function deleteKey(key: string) {
-    map.delete(key);
-  }
+    window.addEventListener("beforeunload", saveCache);
+    return () => window.removeEventListener("beforeunload", saveCache);
+  }, []);
 
-  function keys(): IterableIterator<string> {
-    return map.keys();
-  }
-
-  return () => ({
-    get,
-    set,
-    ["delete"]: deleteKey,
-    keys,
-  });
+  return () => cache.current;
 }
+
+const loadCache = () => localStorage.getItem(CACHE_KEY_NAME);
+
+const storeCache = (c: any) => localStorage.setItem(CACHE_KEY_NAME, c);
