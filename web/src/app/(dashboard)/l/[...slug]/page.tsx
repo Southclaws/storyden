d765f723151669849530e6c54be42e99bda1a3f3
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-10-28 19:26:43 +0000

    add basic opengraph images to library pages

--- web/src/app/(dashboard)/l/[...slug]/page.tsx
+++ web/src/app/(dashboard)/l/[...slug]/page.tsx
@@ -1,10 +1,14 @@
+import { Metadata } from "next";
+
 import { nodeGet } from "@/api/openapi-server/nodes";
 import { getTargetSlug } from "@/components/library/utils";
 import { UnreadyBanner } from "@/components/site/Unready";
+import { WEB_ADDRESS } from "@/config";
+import { getSettings } from "@/lib/settings/settings-server";
 import { LibraryPageScreen } from "@/screens/library/LibraryPageScreen/LibraryPageScreen";
 import { Params, ParamsSchema } from "@/screens/library/library-path";
 
-type Props = {
+export type Props = {
   params: Promise<Params>;
 };
 
@@ -24,5 +28,37 @@ export default async function Page(props: Props) {
     return <LibraryPageScreen node={data} />;
   } catch (e) {
     return <UnreadyBanner error={e} />;
+  }
+}
+
+export async function generateMetadata(props: Props) {
+  try {
+    const { slug } = ParamsSchema.parse(await props.params);
+
+    const targetSlug = getTargetSlug(slug);
+
+    if (!targetSlug) {
+      // NOTE: This state is probably not possible to reach due to the params.
+      throw new Error("Library page not found");
+    }
+
+    const settings = await getSettings();
+
+    const { data } = await nodeGet(targetSlug);
+
+    return {
+      title: `${data.name} | ${settings.title}`,
+      description: data.description,
+      openGraph: {
+        // NOTE: Massive hack because Next.js still hasn't fixed a bug with
+        // catch-all routes and opengraph-image route handlers.
+        images: [`${WEB_ADDRESS}/l/og?slug=${targetSlug}&t=${data.updatedAt}`],
+      },
+    } satisfies Metadata;
+  } catch (e) {
+    return {
+      title: "Page not found",
+      description: "The page you are looking for does not exist.",
+    };
   }
 }
---
+++ web/src/app/(dashboard)/l/og/route.tsx
@@ -0,0 +1,98 @@
+import { ImageResponse } from "next/og";
+import { NextRequest, NextResponse } from "next/server";
+
+import { nodeGet } from "@/api/openapi-server/nodes";
+import { getTargetSlug } from "@/components/library/utils";
+import { getSettings } from "@/lib/settings/settings-server";
+import { ParamsSchema } from "@/screens/library/library-path";
+import { getAssetURL } from "@/utils/asset";
+
+import { Props } from "../[...slug]/page";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export async function GET(req: NextRequest) {
+  const slug = req.nextUrl.searchParams.get("slug");
+  if (!slug) {
+    return new NextResponse("No slug provided", { status: 400 });
+  }
+
+  const { data } = await nodeGet(slug);
+
+  const { accent_colour } = await getSettings();
+
+  const image =
+    data.primary_image?.parent ??
+    data.link?.primary_image ??
+    data.primary_image;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          backgroundColor: accent_colour,
+          fontSize: 16,
+        }}
+      >
+        {image ? (
+          // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+          <img
+            src={getAssetURL(image.path)}
+            width="100%"
+            height="100%"
+            style={{
+              objectPosition: "center",
+              objectFit: "cover",
+            }}
+          />
+        ) : (
+          <div
+            style={{
+              position: "absolute",
+              bottom: 0,
+              display: "flex",
+              flexDirection: "column",
+              padding: "2rem",
+              width: "100%",
+              backgroundColor: "hsla(180deg, 10%, 10%, 0.58)",
+              color: "white",
+            }}
+          >
+            <div
+              style={{
+                fontSize: "4rem",
+                fontWeight: 600,
+              }}
+            >
+              {data.name}
+            </div>
+
+            <div
+              style={{
+                fontSize: "2rem",
+                fontWeight: 300,
+              }}
+            >
+              {data.description}
+            </div>
+          </div>
+        )}
+      </div>
+    ),
+    {
+      ...size,
+      emoji: "fluent",
+    },
+  );
+}
