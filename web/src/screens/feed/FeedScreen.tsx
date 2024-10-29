4efa55eef96e4e44484bf1f1eb7cc9e4c5a7ed08
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-10-29 22:48:59 +0000

    remove janky not-quite-components causing missed throws when API errors occur

--- web/src/screens/feed/FeedScreen.tsx
+++ web/src/screens/feed/FeedScreen.tsx
@@ -3,6 +3,7 @@ import { nodeList } from "@/api/openapi-server/nodes";
 import { threadList } from "@/api/openapi-server/threads";
 import { getServerSession } from "@/auth/server-session";
 import { FeedConfig } from "@/components/feed/FeedConfig/FeedConfig";
+import { UnreadyBanner } from "@/components/site/Unready";
 import { Button } from "@/components/ui/button";
 import { Settings } from "@/lib/settings/settings";
 import { HStack, LStack } from "@/styled-system/jsx";
@@ -32,15 +33,26 @@ async function FeedScreenContent({ initialSettings }: Props) {
 
   switch (feedConfig.source.type) {
     case "threads":
-      return (async () => {
-        const threads = await threadList();
-        return <ThreadFeedScreen initialData={threads.data} />;
-      })();
+      return <ThreadFeedScreenContent />;
 
     case "library":
-      return (async () => {
-        const nodes = await nodeList();
-        return <LibraryFeedScreen initialData={nodes.data} />;
-      })();
+      return <LibraryFeedScreenContent />;
+  }
+}
+
+async function ThreadFeedScreenContent() {
+  try {
+    const threads = await threadList();
+    return <ThreadFeedScreen initialData={threads.data} />;
+  } catch (e) {
+    return <UnreadyBanner error={e} />;
+  }
+}
+async function LibraryFeedScreenContent() {
+  try {
+    const nodes = await nodeList();
+    return <LibraryFeedScreen initialData={nodes.data} />;
+  } catch (e) {
+    return <UnreadyBanner error={e} />;
   }
 }
