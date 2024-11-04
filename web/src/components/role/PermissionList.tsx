8343ab91ae13ed29d9e2dcda94be3ae8e6e61369
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-11-04 11:34:52 +0000

    add permission list popover to roles list

---
+++ web/src/components/role/PermissionList.tsx
@@ -0,0 +1,49 @@
+import { PermissionList } from "@/api/openapi-schema";
+import * as Popover from "@/components/ui/popover";
+import { PermissionDetails } from "@/lib/permission/permission";
+import { LStack } from "@/styled-system/jsx";
+
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+
+type Props = {
+  permissions: PermissionList;
+};
+
+export function PermissionSummary({ permissions }: Props) {
+  const permissionCount = permissions.length;
+
+  const permissionCountLabel =
+    permissionCount === 1 ? "permission" : "permissions";
+
+  const permissionLabel = `${permissionCount} ${permissionCountLabel}`;
+
+  return (
+    <Popover.Root
+      positioning={{
+        slide: true,
+        shift: 16,
+      }}
+    >
+      <Popover.Trigger asChild>
+        <Button size="xs" variant="link">
+          {permissionLabel}
+        </Button>
+      </Popover.Trigger>
+      <Popover.Positioner>
+        <Popover.Content p="2" borderRadius="2xl">
+          <Popover.Arrow>
+            <Popover.ArrowTip />
+          </Popover.Arrow>
+          <Popover.Description>
+            <LStack overflowY="scroll">
+              {permissions.map((p) => (
+                <Badge key={p}>{PermissionDetails[p].name}</Badge>
+              ))}
+            </LStack>
+          </Popover.Description>
+        </Popover.Content>
+      </Popover.Positioner>
+    </Popover.Root>
+  );
+}
