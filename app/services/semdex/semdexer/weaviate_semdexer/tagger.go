87a75ee892a5bf10a35bbfe5053f0fb3c9f293ea
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-11-08 18:51:16 +0000

    limit the tags in the prompt and do a bit of proompt engineering to improve output (im proooompting!!!)

--- app/services/semdex/semdexer/weaviate_semdexer/tagger.go
+++ app/services/semdex/semdexer/weaviate_semdexer/tagger.go
@@ -10,15 +10,20 @@ import (
 	"github.com/Southclaws/fault/fctx"
 	"github.com/Southclaws/storyden/app/resources/datagraph"
 	"github.com/Southclaws/storyden/app/resources/tag/tag_ref"
+	"github.com/samber/lo"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/graphql"
 )
 
-var SuggestTagsPrompt = template.Must(template.New("").Parse(`Analyze the provided content of \"{name}\" and generate relevant tags. Tags are either single words or multiple words separated only by a hyphen, no spaces. Consider the following list of existing tags on the platform for context and prioritization: {{ .AvailableTags }}
+var SuggestTagsPrompt = template.Must(template.New("").Parse(`Analyze the provided content of \"{name}\" and generate relevant tags. Tags are either single words or multiple words separated only by a hyphen, no spaces.
+
+It's very important that only tags that are relevant to the content are returned, any tags of low confidence MUST be omitted. Do not generate tags that are too vague or tags that are too specific and cannot easily be used in other contexts for other types of content. Generally avoid tags that are singular and not plural that too closely match phrases or words in the content.
 
 Suggested Tags: Suggest any existing tags from the list above that best describe this content, prioritizing tags that closely match the main themes, ideas, or entities mentioned.
 New Tags: If there are no suitable matches or if additional tags could enhance discoverability, suggest up to three new tags not found in the existing list. Ensure the tags are relevant, concise, and enhance content discoverability.
 
 Output Format: Provide only a list of tags separated by commas with no additional text or symbols. Suggested tags should come first, followed by new tags, if any.
+
+Consider the following list of existing tags on the platform for context and prioritization: {{ .AvailableTags }}
 
 Content:
 
@@ -26,9 +31,12 @@ Content:
 `))
 
 func (s *weaviateRefIndex) SuggestTags(ctx context.Context, content datagraph.Content, available tag_ref.Names) (tag_ref.Names, error) {
+	// cap the available tags at 50, we don't to blow out the prompt size limit.
+	sliced := lo.Splice(available, 50)
+
 	template := strings.Builder{}
 	err := SuggestTagsPrompt.Execute(&template, map[string]any{
-		"AvailableTags": available,
+		"AvailableTags": sliced,
 		"Content":       content.Plaintext(),
 	})
 	if err != nil {
