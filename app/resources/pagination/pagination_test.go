38bf30531f02141ad1b2e63d40f33c13061e573d
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-11-12 10:39:17 +0000

    fix pagination tests and cover weaviate edge case

--- app/resources/pagination/pagination_test.go
+++ app/resources/pagination/pagination_test.go
@@ -1,8 +1,10 @@
 package pagination
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/Southclaws/dt"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 )
@@ -45,11 +47,11 @@ func TestNewPageResult(t *testing.T) {
 
 		r := NewPageResult(p, tableSize, rows)
 
-		a.Equal(Size(10), r.Size)
+		a.Equal(10, r.Size)
 		a.Equal(10, r.Results)
 		a.Equal(13, r.TotalPages)
-		a.Equal(Page(1), r.CurrentPage)
-		a.Equal(Page(2), r.NextPage.OrZero())
+		a.Equal(1, r.CurrentPage)
+		a.Equal(2, r.NextPage.OrZero())
 		a.Len(r.Items, 10)
 	})
 
@@ -60,11 +62,11 @@ func TestNewPageResult(t *testing.T) {
 
 		r := NewPageResult(p, tableSize, rows)
 
-		a.Equal(Size(10), r.Size)
+		a.Equal(10, r.Size)
 		a.Equal(10, r.Results)
 		a.Equal(13, r.TotalPages)
-		a.Equal(Page(7), r.CurrentPage)
-		a.Equal(Page(8), r.NextPage.OrZero())
+		a.Equal(7, r.CurrentPage)
+		a.Equal(8, r.NextPage.OrZero())
 		a.Len(r.Items, 10)
 	})
 
@@ -75,11 +77,52 @@ func TestNewPageResult(t *testing.T) {
 
 		r := NewPageResult(p, tableSize, rows)
 
-		a.Equal(Size(10), r.Size)
+		a.Equal(10, r.Size)
 		a.Equal(3, r.Results)
 		a.Equal(13, r.TotalPages)
-		a.Equal(Page(13), r.CurrentPage)
+		a.Equal(13, r.CurrentPage)
 		a.False(r.NextPage.Ok(), "there are no more pages after 13")
 		a.Len(r.Items, 3, "the final page contains the final 3 items")
 	})
+
+	t.Run("weaviate_bug", func(t *testing.T) {
+		p := NewPageParams(1, 10)
+
+		// due to complexities of vector searches, sometimes the total count and
+		// the actual query don't agree, the total may be in the hundreds but an
+		// actual query yields a single page with fewer items than the page size
+		// due to autocut and other things.
+		rows := lo.Slice(database, p.Offset(), p.Offset()+3)
+
+		r := NewPageResult(p, tableSize, rows)
+
+		a.Equal(10, r.Size)
+		a.Equal(3, r.Results)
+		a.Equal(1, r.TotalPages)
+		a.Equal(1, r.CurrentPage)
+		a.False(r.NextPage.Ok(), "there are no more pages after 1")
+		a.Len(r.Items, 3, "only 3 items are returned")
+	})
+}
+
+func TestConvertPageResult(t *testing.T) {
+	a := assert.New(t)
+
+	tableSize := 123
+	database := lo.Range(tableSize)
+
+	p := NewPageParams(1, 10)
+	rows := lo.Slice(database, p.Offset(), p.Offset()+p.Limit())
+
+	r := NewPageResult(p, tableSize, rows)
+
+	mapped := dt.Map(r.Items, func(r int) string { return fmt.Sprint(r) })
+
+	converted := ConvertPageResult(r, mapped)
+	a.Equal(10, converted.Size)
+	a.Equal(10, converted.Results)
+	a.Equal(13, converted.TotalPages)
+	a.Equal(1, converted.CurrentPage)
+	a.Equal(2, converted.NextPage.OrZero())
+	a.Len(converted.Items, 10)
 }
