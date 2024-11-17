5948da5c7f273765e20d01e64b180d3b8eb12833
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2024-11-17 16:18:20 +0000

    refresh settings screen styles and update for new auth api

---
+++ web/src/screens/settings/MemberAuthenticationSettingsScreen.tsx
@@ -0,0 +1,5 @@
+import { AuthMethodSettings } from "@/components/settings/AuthMethodSettings/AuthMethodSettings";
+
+export function MemberAuthenticationSettingsScreen() {
+  return <AuthMethodSettings />;
+}
--- web/src/screens/settings/SettingsScreen.tsx
+++ web/src/screens/settings/SettingsScreen.tsx
@@ -1,17 +1,50 @@
 "use client";
 
-import { Heading } from "@/components/ui/heading";
-import { VStack } from "@/styled-system/jsx";
+import { TabsValueChangeDetails } from "@ark-ui/react";
+import { useQueryState } from "nuqs";
+import { useEffect } from "react";
 
-import { AuthMethodSettings } from "./components/AuthMethodSettings/AuthMethodSettings";
-import { IdentitySettings } from "./components/IdentitySettings/IdentitySettings";
+import * as Tabs from "@/components/ui/tabs";
+
+import { MemberAuthenticationSettingsScreen } from "./MemberAuthenticationSettingsScreen";
+
+const DEFAULT_TAB = "authentication";
 
 export function SettingsScreen() {
-  return (
-    <VStack alignItems="start" gap="4">
-      <Heading size="lg">Settings</Heading>
+  const [tab, setTab] = useQueryState("tab", {
+    defaultValue: DEFAULT_TAB,
+  });
 
-      <AuthMethodSettings />
-    </VStack>
+  // NOTE: A hack because for some reason, the tab component renders twice and
+  // the associated hook gets lost and results in `ready` always being false,
+  // despite the useSettings hook returning the correct data. Not sure if this
+  // is a Next.js bug, a React bug or a Ark, Park or something else bug...
+  useEffect(() => {
+    if (!tab) {
+      setTab(DEFAULT_TAB);
+    }
+  }, [tab, setTab]);
+
+  function handleTabChange({ value }: TabsValueChangeDetails) {
+    setTab(value);
+  }
+
+  return (
+    <Tabs.Root
+      width="full"
+      variant="enclosed"
+      defaultValue={DEFAULT_TAB}
+      value={tab}
+      onValueChange={handleTabChange}
+    >
+      <Tabs.List>
+        <Tabs.Trigger value="authentication">Authentication</Tabs.Trigger>
+        <Tabs.Indicator />
+      </Tabs.List>
+
+      <Tabs.Content value="authentication">
+        <MemberAuthenticationSettingsScreen />
+      </Tabs.Content>
+    </Tabs.Root>
   );
 }
