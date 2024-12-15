f777583d8ee22abea2f2ffaee98666d77631c900
barney <barney@deployferry.io>
2024-12-15 15:14:10 -0500

    add a basic algo to content for splitting
    Embeddings work well when individual sentences are indexed instead of entire documents. Currently, the embedding provider will trim at 1k (I think) characters before embedding, which can lose important context and content when indexing.
    
    Eventually, the goal is to split large content pieces into either sentences or, worst case, spans of text bounded by spaces or at pre-set lengths (if no sentence boundaries or spaces can be found)
    
    This commit includes a basic algorithm which operates on `datagraph.Content` to produce a slice of strings representing (roughly) each paragraph of the text. Some extra logic ensures edge cases such as long spans of contiguous characters or long sentences with no punctuation are handled.
    
    The rest of this PR will focus on using the chunked text spans in indexing which will require some internal changes to how vectoring works. Currently, there's an assumption that resources map 1:1 to vector index documents. After this change, this will not be the case as resources will map 1:n where n is the number of text chunks from the original content.

--- app/resources/datagraph/content_test.go
+++ app/resources/datagraph/content_test.go
@@ -125,3 +125,57 @@ func TestNewRichText(t *testing.T) {
 		a.Equal(original, parsed)
 	})
 }
+
+func TestSplit(t *testing.T) {
+	r := require.New(t)
+	a := assert.New(t)
+
+	c, err := NewRichText(`<h1>heading</h1>
+
+<p>Here's a paragraph. It's pretty neat.</p>
+
+<p>Here's the rest of the text.</p>
+
+<img src="http://image.com" />
+
+<p>neat photo right?</p>
+
+<p>This is quite a long post, the summary, should just be the first 128 characters rounded down to the nearest space.</p>`)
+	r.NoError(err)
+	r.NotNil(r)
+
+	ps := c.Split()
+	a.Len(ps, 5)
+}
+
+func TestSplitLong(t *testing.T) {
+	r := require.New(t)
+	a := assert.New(t)
+
+	c, err := NewRichText(`<body>
+<p>A very short paragraph.</p>
+
+<p>A very long single paragraph parato Solis gemitus nefandam munus cupidisque luminis Fuge fuit vestra undis laudando in aristas Lorem markdownum trepidantum genetricis [late](http://hic.com/nunc), steterat delendaque summique non? Domum si cursus supremaque aeraque [manibus](http://www.hoc-quod.io/); Iovis illi fert Bacchum, vulneraque __Oleniae suis__ increpat. Sanguine raucis albet Martius infantemque est _parili multam_ auditaque Caencu inferior augent, vix dote telae volat nec horto sceleratus. Mirator ambage! _Tale_ hic Diomedis, arva sonum factas maxima et relicto Longa incustoditam fixus. Praestantissima operata ardere semine per formae quod: in accipe quamvis amoribus, aquis medio confido puer stridore et clamavit? Video invidiosa Glauci flaventibus funeribus _tenet viscera boumque_ tacuit mearum unda interea calorem poscunt primum. Materia et Ethemon attonitae pactaeque in auctor, furiosior miscuerat? Anguem Arctonque receptus ait vacuus vestigia vapore praetulit, moves? var compression = array + pretestVrmlLion.mediaNetbios(file(topologyFlash, 4), 625909); fat_encoding(4, 1); honeypot += cisc; tigerOutboxFile -= repository_memory_symbolic(skin_dlc(5, sampling_excel)); var floppySmtp = core(5, fiber_opengl_touchscreen, gigabitProxy + rdf_cycle_scrolling); In orare ut finitimi Cum essemus spisso inferias: quam post: __unus cum tendebat__, ad, ecquem. Putaret vulnera noxque de vadorum materiam nomen, nives inroravere equidem Hippason hic convivia: per. var reimage_pup_case = saas + 2; cache = delExecutableMotion.subdirectory_service_status(-3, nosqlBin, analog_thick_rte) - adf; var ddlKerning = sector / meme + 3; Inania classes te lavere; _decimo iter prominet_ Scirone, contra harena! Neu multaque vocatur raucum dux pia, fruges illam Cupidinis huius, corruit aurea crudelius structis. Velamina manebit, unda desint saetas, deteriora domumque: e haec Ceres. Sic duro qua quidque victi Ino non, valentes fuit. Ventisque habeto [aliter feto vinci](http://partibus.org/)! Tonitrumque fidas quaerensque proles temptatum citharae iuguli duae patrio coniugis est mea genus dominae, ut nisi [ignoro](http://letiferos.net/auratis-humili.php)? Aetatis lactantia; ad et per clivo cognovit pretium. Adorat Solem illa flumine nobis patriam auxiliare illa Theseus dubioque lunae nactus discedere obiecit, e. Where do I begin? Easy. There are lots of problems with current forms of government. Clearly Democracy is not working (see [https:&#x2F;&#x2F;www.youtube.com&#x2F;watch?v=QFgcqB8-AxE] for a pretty short summary of why). Based on the IQ distribution, we have that 50% of people are below 100 IQ. If you go to a university or other decently high skill job, think about the dumbest person at that job. They probably have a 110+ IQ. Now consider that over 50% of people are dumber than that idiot who you hate with all your being. He is probably your boss, that project manager who wants you to play hopscotch with strangers as a team building exercise. Imagine if people just like him and even more stupid could singlehandedly decide on who the next ruler of your country will be. Terrible.So clearly the next best option is to have the decision be made by a more intelligent group of people. Who better than Rabbis? They have studied all their lives, are genetically more likely to have a very high IQ, have shown immense dedication, work ethic, and pure intentions (aside from pricking the penis of male converts, not sure why they do that). It&#x27;s common for them to engage in debates and intellectual discussions with each other, and they are chosen by G-d as His favored people to lead the way forward for humanity.Imagine a society where they are able to choose amongst themselves. Personally I think it would be amazing. The person they choose doesn&#x27;t even have to be a Rabbi or Jewish at all, it could be some random kid. But we need to all trust in their judgment because it is the best one available to us. To keep things fresh it&#x27;s probably best to rotate different Rabbis every year, maybe have one year be Conservative, the next one be Reform, etc. just for the variety and to give them a break. Many of them are senior citizens, we don&#x27;t want them getting exhausted or accelerating neurological issues they might have.</p>
+</body>
+`)
+	r.NoError(err)
+	r.NotNil(r)
+
+	ps := c.Split()
+	r.Len(ps, 15)
+	a.Equal("A very short paragraph.", ps[0])
+	a.Equal("A very long single paragraph parato Solis gemitus nefandam munus cupidisque luminis Fuge fuit vestra undis laudando in aristas Lorem markdownum trepidantum genetricis [late](http://hic.com/nunc), steterat delendaque summique non? Domum si cursus supremaque aeraque [manibus](http://www.hoc-quod.io/)", ps[1])
+	a.Equal("Iovis illi fert Bacchum, vulneraque __Oleniae suis__ increpat. Sanguine raucis albet Martius infantemque est _parili multam_ auditaque Caencu inferior augent, vix dote telae volat nec horto sceleratus. Mirator ambage! _Tale_ hic Diomedis, arva sonum factas maxima et relicto Longa incustoditam fixus", ps[2])
+	a.Equal("Praestantissima operata ardere semine per formae quod: in accipe quamvis amoribus, aquis medio confido puer stridore et clamavit? Video invidiosa Glauci flaventibus funeribus _tenet viscera boumque_ tacuit mearum unda interea calorem poscunt primum. Materia et Ethemon attonitae pactaeque in auctor, furiosior miscuerat", ps[3])
+	a.Equal("Anguem Arctonque receptus ait vacuus vestigia vapore praetulit, moves? var compression = array + pretestVrmlLion.mediaNetbios(file(topologyFlash, 4), 625909); fat_encoding(4, 1); honeypot += cisc; tigerOutboxFile -= repository_memory_symbolic(skin_dlc(5, sampling_excel))", ps[4])
+	a.Equal("var floppySmtp = core(5, fiber_opengl_touchscreen, gigabitProxy + rdf_cycle_scrolling); In orare ut finitimi Cum essemus spisso inferias: quam post: __unus cum tendebat__, ad, ecquem. Putaret vulnera noxque de vadorum materiam nomen, nives inroravere equidem Hippason hic convivia: per. var reimage_pup_case = saas + 2; cache = delExecutableMotion", ps[5])
+	a.Equal("subdirectory_service_status(-3, nosqlBin, analog_thick_rte) - adf; var ddlKerning = sector / meme + 3; Inania classes te lavere; _decimo iter prominet_ Scirone, contra harena! Neu multaque vocatur raucum dux pia, fruges illam Cupidinis huius, corruit aurea crudelius structis. Velamina manebit, unda desint saetas, deteriora domumque: e haec Ceres", ps[6])
+	a.Equal("Sic duro qua quidque victi Ino non, valentes fuit. Ventisque habeto [aliter feto vinci](http://partibus.org/)! Tonitrumque fidas quaerensque proles temptatum citharae iuguli duae patrio coniugis est mea genus dominae, ut nisi [ignoro](http://letiferos.net/auratis-humili.php)? Aetatis lactantia; ad et per clivo cognovit pretium", ps[7])
+	a.Equal("Adorat Solem illa flumine nobis patriam auxiliare illa Theseus dubioque lunae nactus discedere obiecit, e. Where do I begin? Easy. There are lots of problems with current forms of government. Clearly Democracy is not working (see [https://www.youtube.com/watch?v=QFgcqB8-AxE] for a pretty short summary of why)", ps[8])
+	a.Equal("Based on the IQ distribution, we have that 50% of people are below 100 IQ. If you go to a university or other decently high skill job, think about the dumbest person at that job. They probably have a 110+ IQ. Now consider that over 50% of people are dumber than that idiot who you hate with all your being", ps[9])
+	a.Equal("He is probably your boss, that project manager who wants you to play hopscotch with strangers as a team building exercise. Imagine if people just like him and even more stupid could singlehandedly decide on who the next ruler of your country will be. Terrible", ps[10])
+	a.Equal("So clearly the next best option is to have the decision be made by a more intelligent group of people. Who better than Rabbis? They have studied all their lives, are genetically more likely to have a very high IQ, have shown immense dedication, work ethic, and pure intentions (aside from pricking the penis of male converts, not sure why they do", ps[11])
+	a.Equal("that). It's common for them to engage in debates and intellectual discussions with each other, and they are chosen by G-d as His favored people to lead the way forward for humanity.Imagine a society where they are able to choose amongst themselves. Personally I think it would be amazing", ps[12])
+	a.Equal("The person they choose doesn't even have to be a Rabbi or Jewish at all, it could be some random kid. But we need to all trust in their judgment because it is the best one available to us. To keep things fresh it's probably best to rotate different Rabbis every year, maybe have one year be Conservative, the next one be Reform, etc", ps[13])
+	a.Equal("just for the variety and to give them a break. Many of them are senior citizens, we don't want them getting exhausted or accelerating neurological issues they might have", ps[14])
+}
