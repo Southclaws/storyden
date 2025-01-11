487f215741d6aa2da6accc8eab62f1806ce58603
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2025-01-11 22:32:14 +0000

    fix server side hydration of thread feed

--- web/src/screens/feed/ThreadFeedScreen/useThreadFeedScreen.ts
+++ web/src/screens/feed/ThreadFeedScreen/useThreadFeedScreen.ts
@@ -6,14 +6,21 @@ import { Account, Category, ThreadListResult } from "@/api/openapi-schema";
 export type Props = {
   initialSession?: Account;
   initialPage: number;
-  initialPageData?: ThreadListResult[];
+  initialPageData?: ThreadListResult;
   category?: Category;
 };
 
 export function useThreadFeedScreen(props: Props) {
-  const { data, error } = useThreadList({
-    page: props.initialPage.toString(),
-  });
+  const { data, error } = useThreadList(
+    {
+      page: props.initialPage.toString(),
+    },
+    {
+      swr: {
+        fallbackData: props.initialPageData,
+      },
+    },
+  );
   if (!data) {
     return {
       ready: false as const,
