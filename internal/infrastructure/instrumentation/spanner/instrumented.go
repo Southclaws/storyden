2b7da89015622a612a39635e9ae073c90d35943f
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2025-01-14 20:57:51 +0000

    add tracing instrumentation

---
+++ internal/infrastructure/instrumentation/spanner/instrumented.go
@@ -0,0 +1,121 @@
+package spanner
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/Southclaws/fault"
+	"github.com/Southclaws/fault/fctx"
+	"github.com/Southclaws/fault/fmsg"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/Southclaws/storyden/internal/infrastructure/instrumentation/kv"
+	"github.com/Southclaws/storyden/internal/infrastructure/instrumentation/tracing"
+)
+
+// Build is to be used in services during initialisation.
+func (i *service) Build() Instrumentation {
+	pc, _, _, _ := runtime.Caller(1)
+	path := runtime.FuncForPC(pc).Name()
+	caller := filepath.Base(path)
+	pkg := strings.Split(caller, ".")[0]
+	tr := i.tf.Build(i.lc, pkg)
+
+	return &impl{
+		tracer: tr,
+	}
+}
+
+type impl struct {
+	tracer tracing.Tracer
+}
+
+func (i *impl) Instrument(ctx context.Context, a ...kv.Attr) (context.Context, Span) {
+	pkg, fn, loc := getLoc(runtime.Caller(1))
+
+	a = append(a,
+		kv.String("package", pkg),
+		kv.String("location", loc),
+	)
+
+	return i.InstrumentNamed(ctx, fn, a...)
+}
+
+func (i *impl) InstrumentNamed(ctx context.Context, name string, a ...kv.Attr) (context.Context, Span) {
+	// Create a child span with the KV data as attributes.
+	ctx, span := i.tracer.Start(ctx, name, trace.WithAttributes(kv.Attrs(a).ToAttributes()...))
+
+	// Create a new context with the KV data as fctx metadata.
+	ctx = fctx.WithMeta(ctx, kv.Attrs(a).ToFault()...)
+
+	// NOTE: We store the context into the tracking span so it can be mutated
+	// and new child contexts can be returned with the new attributes.
+	return ctx, &trackingSpan{span, ctx, name}
+}
+
+type trackingSpan struct {
+	span trace.Span
+	// NOTE: We store ctx because we need to mutate it in Annotate.
+	//nolint:containedctx
+	ctx    context.Context
+	caller string
+}
+
+func (t *trackingSpan) End() {
+	t.span.End()
+}
+
+func (t *trackingSpan) Annotate(a ...kv.Attr) context.Context {
+	//
+	// NON-OBVIOUS MUTATIONS AHEAD
+	//
+	// We need to mutate the context and the logger to add the attributes.
+	// The reason for this is the ctx and logger are passed in to the Instrument
+	// function (which is always called once at the start of a procedure) and we
+	// don't want to force users to explicitly pass in the ctx and logger for
+	// every single annotation throughout the procedure. This means that when
+	// a span is annotated, we can still return a new context and child logger.
+	//
+	t.ctx = fctx.WithMeta(t.ctx, kv.Attrs(a).ToFault()...)
+
+	// Add the attributes to the span. This is a slightly more obvious mutation.
+	t.span.SetAttributes(kv.Attrs(a).ToAttributes()...)
+
+	return t.ctx
+}
+
+func (t *trackingSpan) Event(name string, a ...kv.Attr) {
+	t.span.AddEvent(name, trace.WithStackTrace(true), trace.WithAttributes(kv.Attrs(a).ToAttributes()...))
+}
+
+func (t *trackingSpan) Wrap(err error, msg string, a ...kv.Attr) error {
+	t.span.SetStatus(codes.Error, msg)
+	t.span.RecordError(err, trace.WithAttributes(kv.Attrs(a).ToAttributes()...))
+
+	ctx := fctx.WithMeta(t.ctx, kv.Attrs(a).ToFault()...)
+
+	return fault.Wrap(err, fctx.With(ctx), fmsg.With(msg))
+}
+
+func getLoc(pc uintptr, file string, line int, ok bool) (string, string, string) {
+	if !ok {
+		return "", "", ""
+	}
+
+	file = file[strings.Index(file, "captain/app")+len("captain/"):]
+	loc := fmt.Sprintf("%s:%d", file, line)
+
+	path := runtime.FuncForPC(pc).Name() // get the fully qualified object-path to the caller function
+	caller := filepath.Base(path)        // get the caller in "package.Function" format
+	sep := strings.Index(caller, ".")    // split `package.Function``
+	if sep == -1 {
+		sep = len(caller)
+	}
+	pkg := caller[:sep] // get package name
+
+	return pkg, caller, loc
+}
---
+++ internal/infrastructure/instrumentation/tracing/factory.go
@@ -0,0 +1,127 @@
+package tracing
+
+import (
+	"context"
+
+	"github.com/Southclaws/fault"
+	"github.com/Southclaws/fault/fmsg"
+	"github.com/getsentry/sentry-go"
+	sentryotel "github.com/getsentry/sentry-go/otel"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+
+	"github.com/Southclaws/storyden/internal/config"
+)
+
+type factory struct {
+	provider string
+	opts     []trace.TracerProviderOption
+}
+
+func Build() fx.Option {
+	return fx.Provide(newExporter, newTracerFactory)
+}
+
+func newTracerFactory(ctx context.Context,
+	cfg config.Config,
+	logger *zap.Logger,
+	opts []trace.TracerProviderOption,
+) (Factory, error) {
+	otel.SetErrorHandler(otel.ErrorHandlerFunc(func(err error) {
+		logger.Error("otel error", zap.String("error", err.Error()))
+	}))
+
+	return factory{
+		provider: cfg.OTELProvider,
+		opts:     opts,
+	}, nil
+}
+
+func newExporter(ctx context.Context,
+	cfg config.Config,
+	logger *zap.Logger,
+) ([]trace.TracerProviderOption, error) {
+	switch cfg.OTELProvider {
+	case "sentry":
+
+		if cfg.SentryDSN == "" {
+			if cfg.OTELEndpoint.String() != "" {
+				return nil, fault.New("OTEL_EXPORTER_OTLP_ENDPOINT is set but sentry DSN is required instead when using the sentry provider")
+			}
+			return nil, fault.New("sentry DSN is required when using the sentry provider")
+		}
+
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn:              cfg.SentryDSN,
+			EnableTracing:    true,
+			TracesSampleRate: 1.0,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		spanProc := sentryotel.NewSentrySpanProcessor()
+
+		// for some reason, sentry is a "span processor" not a "span exporter".
+		return []trace.TracerProviderOption{
+			trace.WithSpanProcessor(spanProc),
+		}, nil
+
+	case "otlp":
+		endpoint := cfg.OTELEndpoint.String()
+		if endpoint == "" {
+			return nil, fault.New("OTEL_EXPORTER_OTLP_ENDPOINT is required when using the otlp provider")
+		}
+
+		opts := []otlptracehttp.Option{
+			otlptracehttp.WithEndpointURL(endpoint),
+		}
+
+		if cfg.OTELEndpoint.Scheme != "https" {
+			opts = append(opts, otlptracehttp.WithInsecure())
+		}
+
+		otlp, err := otlptracehttp.New(ctx, opts...)
+		if err != nil {
+			return nil, fault.Wrap(err, fmsg.With("failed to create OTLP exporter"))
+		}
+
+		return []trace.TracerProviderOption{
+			trace.WithBatcher(otlp),
+		}, nil
+
+	default:
+		return []trace.TracerProviderOption{
+			trace.WithSyncer(newLoggingTracer(logger)),
+		}, nil
+	}
+}
+
+// Build constructs a new tracer for use within a system component.
+func (f factory) Build(lc fx.Lifecycle, serviceName string) Tracer {
+	opts := append(f.opts, trace.WithResource(resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceName(serviceName),
+	)))
+
+	tp := trace.NewTracerProvider(opts...)
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			if err := tp.Shutdown(ctx); err != nil {
+				return fault.Wrap(err)
+			}
+
+			return nil
+		},
+	})
+
+	tracer := tp.Tracer("storyden")
+
+	return tracer
+}
