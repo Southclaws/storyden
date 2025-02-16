e7cc9b51def3dcec8af7b5683b9cd84d17e20ef6
Barney <barney@deployferry.io>
2025-02-16 18:27:11 +0000

    implement basic UI for editing node properties

--- web/src/components/library/LibraryPagePropertyTable/LibraryPagePropertyTable.tsx
+++ web/src/components/library/LibraryPagePropertyTable/LibraryPagePropertyTable.tsx
@@ -1,38 +1,14 @@
 import { uniqueId } from "lodash/fp";
-import { useRef, useState } from "react";
-import {
-  Controller,
-  ControllerFieldState,
-  ControllerProps,
-  ControllerRenderProps,
-  FieldValues,
-  UseFormStateReturn,
-} from "react-hook-form";
+import { ChangeEvent } from "react";
+import { Controller, ControllerProps, FieldValues } from "react-hook-form";
 
-import { handle } from "@/api/client";
-import { tagList } from "@/api/openapi-client/tags";
-import {
-  Identifier,
-  InstanceCapability,
-  Node,
-  NodeWithChildren,
-  Property,
-  PropertyName,
-  PropertySchema,
-  TagNameList,
-} from "@/api/openapi-schema";
-import { IntelligenceAction } from "@/components/site/Action/Intelligence";
-import { TagBadgeList } from "@/components/tag/TagBadgeList";
+import { NodeWithChildren, PropertyName } from "@/api/openapi-schema";
 import { Button } from "@/components/ui/button";
-import { Combotags, CombotagsHandle } from "@/components/ui/combotags";
 import { IconButton } from "@/components/ui/icon-button";
 import { DeleteIcon } from "@/components/ui/icons/Delete";
 import { Input } from "@/components/ui/input";
-import { useLibraryMutation } from "@/lib/library/library";
-import { usePropertyMutation } from "@/lib/library/properties";
-import { useCapability } from "@/lib/settings/capabilities";
 import { Form } from "@/screens/library/LibraryPageScreen/useLibraryPageScreen";
-import { HStack, LStack, styled } from "@/styled-system/jsx";
+import { Center, HStack, LStack, styled } from "@/styled-system/jsx";
 
 export type Props<T extends FieldValues> = Omit<
   ControllerProps<T>,
@@ -42,11 +18,11 @@ export type Props<T extends FieldValues> = Omit<
   node: NodeWithChildren;
 };
 
-export function LibraryPagePropertyTable<T extends FieldValues>({
+export function LibraryPagePropertyTable({
   editing,
   node,
   ...props
-}: Props<T>) {
+}: Props<Form>) {
   if (editing) {
     return (
       <LibraryPagePropertyTableEditable
@@ -56,8 +32,6 @@ export function LibraryPagePropertyTable<T extends FieldValues>({
       />
     );
   }
-
-  console.log(node.properties);
 
   return (
     <styled.dl display="table" borderCollapse="collapse">
@@ -121,15 +95,13 @@ export function LibraryPagePropertyTableEditable({
         const current = fieldValue ?? initialValue ?? [];
 
         async function handleAddProperty() {
-          // await addField({ name: "", type: "string", sort: "99999999" });
-
-          console.log(field, fieldState, formState);
-
           field.onChange([
             ...current,
             {
+              fid: uniqueId("new_field_"),
               name: uniqueId("Field"),
               type: "text",
+              sort: "5",
               value: "",
             },
           ]);
@@ -140,60 +112,72 @@ export function LibraryPagePropertyTableEditable({
           field.onChange(next);
         }
 
-        console.log({
-          defaults: formState.defaultValues,
-          fieldValue,
-          initialValue,
-          current,
-        });
+        function handlePropertyNameChange(name: PropertyName, newName: string) {
+          const next = current.map((f) => {
+            if (f.name === name) {
+              f.name = newName;
+            }
+
+            return f;
+          });
+          field.onChange(next);
+        }
+
+        function handlePropertyValueChange(name: PropertyName, value: string) {
+          const next = current.map((f) => {
+            if (f.name === name) {
+              f.value = value;
+            }
+
+            return f;
+          });
+          field.onChange(next);
+        }
 
         return (
           <LStack w="64">
-            <pre>properties: {JSON.stringify(current)}</pre>
             <styled.dl display="table" borderCollapse="collapse">
               {current.map((p) => {
                 function handleRemove() {
                   handleRemoveProperty(p.name);
                 }
+
+                function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
+                  handlePropertyNameChange(p.name, e.target.value);
+                }
+
+                function handleValueChange(e: ChangeEvent<HTMLInputElement>) {
+                  handlePropertyValueChange(p.name, e.target.value);
+                }
+
                 return (
-                  <HStack key={p.name} display="table-row">
-                    <styled.dt
-                      display="table-cell"
-                      w="full"
-                      p="1"
-                      borderRadius="sm"
-                      textOverflow="ellipsis"
-                      overflowX="hidden"
-                      color="fg.muted"
-                      _hover={{
-                        color: "fg.default",
-                        background: "bg.muted",
-                        cursor: "pointer",
-                      }}
-                    >
-                      <Input defaultValue={p.name} />
+                  <HStack key={p.fid} display="table-row">
+                    <styled.dt display="table-cell" p="1" color="fg.muted">
+                      <Input
+                        variant="ghost"
+                        defaultValue={p.name}
+                        onChange={handleNameChange}
+                      />
                     </styled.dt>
-                    <styled.dd
-                      display="table-cell"
-                      p="1"
-                      w="min"
-                      borderRadius="sm"
-                      _hover={{
-                        color: "fg.default",
-                        background: "bg.muted",
-                        cursor: "pointer",
-                      }}
-                    >
-                      {p.value}
+                    <styled.dd display="table-cell" p="1">
+                      <Input
+                        variant="ghost"
+                        defaultValue={p.value}
+                        onChange={handleValueChange}
+                      />
                     </styled.dd>
 
-                    <IconButton
-                      type="button"
-                      variant="subtle"
-                      onClick={handleRemove}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
+                    <Center>
+                      <IconButton
+                        type="button"
+                        variant="ghost"
+                        color="fg.destructive"
+                        size="sm"
+                        onClick={handleRemove}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </Center>
                   </HStack>
                 );
               })}
