071dd8d8607f8d709da265e2e497102bd425f4b6
TrumanP12 <trumanpatterson12@gmail.com>
2025-04-10 15:33:24 -0500

    like button aria settings

--- web/src/components/site/Action/Like.tsx
+++ web/src/components/site/Action/Like.tsx
@@ -10,7 +10,13 @@ type Props = ButtonProps & { liked: boolean };
 export function LikeAction(props: Props) {
   const { liked, ...rest } = props;
   return (
-    <IconButton variant="subtle" size="xs" {...rest}>
+    <IconButton 
+      variant="subtle" 
+      size="xs" 
+      aria-pressed={liked} 
+      aria-label={liked ? "Unlike" : "Like"}
+      title={liked ? "Unlike" : "Like"}
+      {...rest}>
       {liked ? <LikeSavedIcon /> : <LikeIcon />}
     </IconButton>
   );
