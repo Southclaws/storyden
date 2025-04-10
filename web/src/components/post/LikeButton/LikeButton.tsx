4dc48e158f44de726b59672d66ea82b00ea239b1
nazarit19 <nazarit19@iastate.edu>
2025-04-10 00:28:05 -0500

    code style consistent with codebase, cleaned up

--- web/src/components/post/LikeButton/LikeButton.tsx
+++ web/src/components/post/LikeButton/LikeButton.tsx
@@ -2,13 +2,11 @@ import { Box } from "@/styled-system/jsx";
 
 import { Props, useLikeButton } from "./useLikeButton";
 import { LikeAction } from "@/components/site/Action/Like";
-import { useFeedMutations } from "@/lib/feed/mutation";
-import { handle } from "@/api/client";
 
 export function LikeButton(props: Props) {
 
   const { handleClick } = useLikeButton(
-    {thread: props.thread}
+    {thread: props.thread},
   );
 
   return (
