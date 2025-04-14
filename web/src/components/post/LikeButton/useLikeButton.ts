3a26d6ba0118430667e3e8681b07beed82a834de
Kaizzky <daitran@iastate.edu>
2025-04-14 12:34:57 -0500

    extract cleanup handler, and clean up formatting

--- web/src/components/post/LikeButton/useLikeButton.ts
+++ web/src/components/post/LikeButton/useLikeButton.ts
@@ -3,38 +3,36 @@ import { PostReference } from "src/api/openapi-schema";
 import { handle } from "@/api/client";
 import { useFeedMutations } from "@/lib/feed/mutation";
 
-export type Props = {
-  thread: PostReference;
+export type UseLikeButtonProps = {
+ thread: PostReference;
 };
 
-export function useLikeButton({ thread }: Props) {
-  const { likePost, unlikePost, revalidate } = useFeedMutations();
+export function useLikeButton({ thread }: UseLikeButtonProps) {
+ const { likePost, unlikePost, revalidate } = useFeedMutations();
 
-  const handleClick = async () => {
-    // This is a toggle button, so we'll either like or unlike depending on what they have now.
-    if (thread.likes.liked) {
-        await handle(
-          async () => {
-            await unlikePost(thread.id);
-          },
-          {
-            cleanup: async () => await revalidate(),
-          },
-        );
-    } else {
-      await handle(
-        async () => {
-          await likePost(thread.id);
-        },
-        {
-          cleanup: async () => await revalidate(),
-        },
-      );
-    }
-  }
+ const cleanupHandler = async () => revalidate();
 
-  return {
-    ready: true as const,
-    handleClick
-  };
+ const handleClick = async () => {
+   // This is a toggle button, so we'll either like or unlike depending on what they have now.
+   if (thread.likes.liked) {
+     await handle(
+       async () => {
+         await unlikePost(thread.id);
+       },
+       { cleanup: cleanupHandler }
+     );
+   } else {
+     await handle(
+       async () => {
+         await likePost(thread.id);
+       },
+       { cleanup: cleanupHandler }
+     );
+   }
+ };
+
+ return {
+   ready: true as const,
+   handleClick,
+ };
 }
