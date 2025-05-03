d470e46ce822893a88d19ca110f0226c8eaadf05
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2025-05-03 21:09:50 +0100

    fix #404

--- web/src/app/(bare)/login/page.tsx
+++ web/src/app/(bare)/login/page.tsx
@@ -1,11 +1,21 @@
 import { LoginScreen } from "src/screens/auth/LoginScreen/LoginScreen";
 
+import { OAuthProviderList } from "@/components/auth/OAuthProviderList";
 import { UnreadyBanner } from "@/components/site/Unready";
 import { getSettings } from "@/lib/settings/settings-server";
 
+// NOTE: We don't want any caching for data fetching here. OAuth URLs need to be
+// generated freshly for each page render.
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   try {
-    return <LoginScreen />;
+    return (
+      <>
+        <LoginScreen />
+        <OAuthProviderList />
+      </>
+    );
   } catch (error) {
     return <UnreadyBanner error={error} />;
   }
