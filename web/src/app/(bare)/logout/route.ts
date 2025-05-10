7548d1f81a82b4defdb65245b9de287438913df4
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2025-05-10 22:46:37 +0100

    fix logout
    https://southcla.ws/how-to-implement-logout-nextjs-cache-revalidate

--- web/src/app/(bare)/logout/route.ts
+++ web/src/app/(bare)/logout/route.ts
@@ -1,12 +1,21 @@
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { WEB_ADDRESS } from "src/config";
 
+const cookieName = "storyden-session";
+
 export async function GET() {
+  revalidatePath("/", "layout");
+
+  (await cookies()).delete(cookieName);
+
   return NextResponse.redirect(WEB_ADDRESS, {
     headers: {
       "Clear-Site-Data": `"*"`,
       "Set-Cookie": "storyden-session=",
+      "Cache-Control": "no-store",
     },
   });
 }
