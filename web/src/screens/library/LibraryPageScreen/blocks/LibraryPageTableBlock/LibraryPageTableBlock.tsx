fcd3456ca2b8d3a40108d5cb29ed7c18cfee51d3
Barnaby Keene <1636971+Southclaws@users.noreply.github.com>
2025-06-02 20:41:29 +0100

    merge node fields and properties in table

--- web/src/screens/library/LibraryPageScreen/blocks/LibraryPageTableBlock/LibraryPageTableBlock.tsx
+++ web/src/screens/library/LibraryPageScreen/blocks/LibraryPageTableBlock/LibraryPageTableBlock.tsx
@@ -1,19 +1,182 @@
 import { SortableContext } from "@dnd-kit/sortable";
 
 import { useNodeListChildren } from "@/api/openapi-client/nodes";
-import { PropertySchema } from "@/api/openapi-schema";
+import {
+  Identifier,
+  Node,
+  NodeWithChildren,
+  PropertyName,
+  PropertySchema,
+  PropertySchemaList,
+  PropertyType,
+  PropertyValue,
+} from "@/api/openapi-schema";
 import {
   SortIndicator,
   useSortIndicator,
 } from "@/components/site/SortIndicator";
 import { Unready } from "@/components/site/Unready";
 import * as Table from "@/components/ui/table";
+import {
+  LibraryPageBlockTypeTable,
+  LibraryPageBlockTypeTableConfig,
+} from "@/lib/library/metadata";
 import { Box } from "@/styled-system/jsx";
 
 import { useLibraryPageContext } from "../../Context";
 
+type ColumnDefinitionCommon = {
+  fid: Identifier;
+  name: PropertyName;
+  type: PropertyType;
+  hidden: boolean;
+};
+
+type ColumnDefinitionProperty = ColumnDefinitionCommon & {
+  _fixedFieldName?: undefined;
+};
+
+type ColumnDefinitionFixed = ColumnDefinitionCommon & {
+  fid: `fixed:${MappableNodeField}`;
+  name: MappableNodeField; // TODO: Pretty names for fixed fields
+  _fixedFieldName: MappableNodeField;
+};
+
+type ColumnDefinition = ColumnDefinitionProperty | ColumnDefinitionFixed;
+
+type ColumnValue = {
+  fid: Identifier;
+  value: PropertyValue;
+};
+
+type MappableNodeField = Extract<
+  "slug" | "name" | "link" | "description",
+  keyof NodeWithChildren
+>;
+
+const MappableNodeFields: Array<MappableNodeField> = [
+  "slug",
+  "name",
+  "link",
+  "description",
+];
+
+type ProcessedConfig = {
+  columns: Array<ColumnDefinition>;
+};
+
+function getDefaultBlockConfig(ps: PropertySchemaList): ProcessedConfig {
+  const fixedCols: ColumnDefinitionFixed[] = MappableNodeFields.map(
+    (field) =>
+      ({
+        fid: `fixed:${field}`,
+        name: field,
+        type: "text" as const,
+        hidden: false,
+        _fixedFieldName: field as MappableNodeField,
+      }) satisfies ColumnDefinitionFixed,
+  );
+
+  const propCols: ColumnDefinitionProperty[] = ps.map((property) => ({
+    fid: property.fid,
+    name: property.name,
+    type: property.type,
+    sort: property.sort,
+    hidden: false,
+  }));
+
+  const columns: ColumnDefinition[] = [...fixedCols, ...propCols];
+
+  return {
+    columns,
+  };
+}
+
+function processBlockConfig(
+  config: LibraryPageBlockTypeTableConfig,
+): ProcessedConfig {
+  const columns: ColumnDefinition[] = config.columns.map((column) => {
+    if (column.fid.startsWith("fixed:")) {
+      return {
+        fid: column.fid as `fixed:${MappableNodeField}`,
+        name: column.name as MappableNodeField, // TODO: Pretty names for fixed fields
+        type: "text" as const, // Fixed fields are always text for now
+        hidden: column.hidden,
+        _fixedFieldName: column.fid.replace("fixed:", "") as MappableNodeField,
+      } satisfies ColumnDefinitionFixed;
+    }
+
+    return column as ColumnDefinitionProperty;
+  });
+
+  return {
+    ...config,
+    columns,
+  };
+}
+
+function mergeFieldsAndPropertySchema(
+  node: NodeWithChildren,
+  block: LibraryPageBlockTypeTable,
+): ColumnDefinition[] {
+  const config =
+    block.config === undefined
+      ? getDefaultBlockConfig(node.child_property_schema)
+      : processBlockConfig(block.config);
+
+  const columns: ColumnDefinition[] = config.columns.map((column) => {
+    const r: ColumnDefinition = {
+      fid: column.fid,
+      name: column.name,
+      type: column.type as PropertyType,
+      hidden: column.hidden ?? false,
+      _fixedFieldName: undefined,
+    };
+
+    return r;
+  });
+
+  return columns;
+}
+
+function mergeFieldsAndProperties(
+  schema: PropertySchemaList,
+  node: NodeWithChildren,
+  block: LibraryPageBlockTypeTable,
+): ColumnValue[] {
+  const config =
+    block.config === undefined
+      ? getDefaultBlockConfig(schema)
+      : processBlockConfig(block.config);
+
+  const columns: ColumnValue[] = config.columns.map((column) => {
+    if (column._fixedFieldName) {
+      if (column._fixedFieldName === "link") {
+        return {
+          fid: column.fid,
+          value: node.link?.url ?? "",
+        };
+      }
+
+      return {
+        fid: column.fid,
+        value: node[column._fixedFieldName] ?? "",
+      };
+    } else {
+      const value = node.properties.find((p) => p.fid === column.fid)?.value;
+
+      return {
+        fid: column.fid,
+        value: value ?? "",
+      };
+    }
+  });
+
+  return columns;
+}
+
 export function LibraryPageTableBlock() {
-  const { node } = useLibraryPageContext();
+  const { node, form } = useLibraryPageContext();
   const { sort, handleSort } = useSortIndicator();
 
   // format the sort property as "name" or "-name" for asc/desc
@@ -42,15 +205,18 @@ export function LibraryPageTableBlock() {
     return null;
   }
 
-  const columns = [
-    {
-      name: "Name",
-      fid: "name",
-      sort: "0",
-      type: "text",
-    } satisfies PropertySchema,
-    ...node.child_property_schema,
-  ];
+  const currentMeta = form.watch("meta");
+
+  const block = currentMeta.layout?.blocks.find((b) => b.type === "table");
+
+  if (!block) {
+    console.warn(
+      "attempting to render a LibraryPageTableBlock without a block in the form metadata",
+    );
+    return null;
+  }
+
+  const columns = mergeFieldsAndPropertySchema(node, block);
 
   return (
     <Table.Root size="sm" variant="dense">
@@ -102,18 +268,25 @@ export function LibraryPageTableBlock() {
       </Table.Head>
       <Table.Body>
         <SortableContext items={nodes}>
-          {nodes.map((child) => (
-            <Table.Row key={child.id}>
-              <Table.Cell fontWeight="medium">{child.name}</Table.Cell>
-              {node.child_property_schema.map((property) => {
-                const value = child.properties.find(
-                  (p) => p.fid === property.fid,
-                )?.value;
+          {nodes.map((child) => {
+            const columns = mergeFieldsAndProperties(
+              node.child_property_schema,
+              child,
+              block,
+            );
 
-                return <Table.Header key={property.fid}>{value}</Table.Header>;
-              })}
-            </Table.Row>
-          ))}
+            console.log(columns);
+
+            return (
+              <Table.Row key={child.id}>
+                {columns.map((column) => {
+                  return (
+                    <Table.Header key={column.fid}>{column.value}</Table.Header>
+                  );
+                })}
+              </Table.Row>
+            );
+          })}
         </SortableContext>
       </Table.Body>
     </Table.Root>
