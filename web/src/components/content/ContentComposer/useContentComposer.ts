b75cf68a9565b92eaf7b52d5d410430bf80253e9
Barnaby Keene <hello@southcla.ws>
2025-06-10 19:53:24 +0100

    expose class name in composer and don't default to full height

--- web/src/components/content/ContentComposer/useContentComposer.ts
+++ web/src/components/content/ContentComposer/useContentComposer.ts
@@ -21,6 +21,7 @@ import { ImageExtended } from "./plugins/ImagePlugin";
 export type Block = "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 export type ContentComposerProps = {
+  className?: string;
   disabled?: boolean;
   resetKey?: string;
   initialValue?: string;
