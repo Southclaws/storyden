182d426d940a89f4276f3897795421655a81cad2
Barnaby Keene <hello@southcla.ws>
2025-06-24 18:57:06 +0100

    add auth to mcp

--- app/transports/mcp/server.go
+++ app/transports/mcp/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/mark3labs/mcp-go/server"
 
 	"github.com/Southclaws/storyden/app/transports/http/middleware/reqlog"
+	"github.com/Southclaws/storyden/app/transports/http/middleware/session_cookie"
 	"github.com/Southclaws/storyden/app/transports/mcp/tools"
 	"github.com/Southclaws/storyden/internal/config"
 )
@@ -27,11 +28,12 @@ func New(cfg config.Config, allTools tools.All) *server.SSEServer {
 func MountMCP(
 	mux *http.ServeMux,
 	s *server.SSEServer,
+	cj *session_cookie.Jar,
 	reqlog *reqlog.Middleware, // TODO: Make Flushable
 ) {
 	// TODO: Add middleware to this (until we fix oapi-codegen mount)
 	// also, ensure every request requires auth, since the ctx checks are used
 	// in handlers to perform RBAC checks already so no auth will just error.
 	// TODO: Introduce config MCP_ENABLED default to disabled
-	mux.Handle("/mcp/", s)
+	mux.Handle("/mcp/", cj.WithAuth()(s))
 }
