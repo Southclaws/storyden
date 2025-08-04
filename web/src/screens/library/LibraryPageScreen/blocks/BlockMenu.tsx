1d78d8d17622600ba67d6d5da25068176f69339e
Barnaby Keene <hello@southcla.ws>
2025-08-04 19:01:38 +0100

    remove unnecessary forwardRef from BlockMenu

--- web/src/screens/library/LibraryPageScreen/blocks/BlockMenu.tsx
+++ web/src/screens/library/LibraryPageScreen/blocks/BlockMenu.tsx
@@ -1,5 +1,5 @@
 import { MenuSelectionDetails, Portal } from "@ark-ui/react";
-import { PropsWithChildren, forwardRef } from "react";
+import { PropsWithChildren } from "react";
 
 import { ButtonProps } from "@/components/ui/button";
 import { DeleteIcon } from "@/components/ui/icons/Delete";
@@ -18,59 +18,57 @@ type Props = {
 
 type AllProps = PropsWithChildren<Props & ButtonProps>;
 
-export const BlockMenu = forwardRef<HTMLDivElement, AllProps>(
-  ({ children, open, block }: AllProps, ref) => {
-    const emit = useEmitLibraryBlockEvent();
+export function BlockMenu({ children, open, block }: AllProps) {
+  const emit = useEmitLibraryBlockEvent();
 
-    function handleSelect(value: MenuSelectionDetails) {
-      switch (value.value) {
-        case "delete": {
-          emit("library:remove-block", {
-            type: block.type,
-          });
-        }
+  function handleSelect(value: MenuSelectionDetails) {
+    switch (value.value) {
+      case "delete": {
+        emit("library:remove-block", {
+          type: block.type,
+        });
       }
     }
+  }
 
-    return (
-      <Menu.Root
-        open={open}
-        lazyMount
-        onSelect={handleSelect}
-        positioning={{
-          placement: "right-start",
-          gutter: 0,
-        }}
-      >
-        <Menu.Trigger asChild>
-          {/*  */}
-          {children}
-        </Menu.Trigger>
+  return (
+    <Menu.Root
+      open={open}
+      lazyMount
+      onSelect={handleSelect}
+      positioning={{
+        placement: "right-start",
+        gutter: 0,
+      }}
+    >
+      <Menu.Trigger asChild>
+        {/*  */}
+        {children}
+      </Menu.Trigger>
 
-        <Portal>
-          <Menu.Positioner ref={ref}>
-            <Menu.Content minW="36">
-              <Menu.ItemGroup>
-                <Menu.ItemGroupLabel
-                  display="flex"
-                  flexDir="column"
-                  userSelect="none"
-                >
-                  <styled.span>{LibraryPageBlockName[block.type]}</styled.span>
-                </Menu.ItemGroupLabel>
+      <Portal>
+        <Menu.Positioner>
+          <Menu.Content minW="36">
+            <Menu.ItemGroup>
+              <Menu.ItemGroupLabel
+                display="flex"
+                flexDir="column"
+                userSelect="none"
+              >
+                <styled.span>{LibraryPageBlockName[block.type]}</styled.span>
+              </Menu.ItemGroupLabel>
 
-                <Menu.Separator />
+              <Menu.Separator />
 
-                <Menu.Item value="delete">
-                  <DeleteIcon />
-                  &nbsp;Delete
-                </Menu.Item>
-                <CreateBlockMenu />
-              </Menu.ItemGroup>
-            </Menu.Content>
-          </Menu.Positioner>
-        </Portal>
-      </Menu.Root>
-    );
-  },
-);
+              <Menu.Item value="delete">
+                <DeleteIcon />
+                &nbsp;Delete
+              </Menu.Item>
+              <CreateBlockMenu />
+            </Menu.ItemGroup>
+          </Menu.Content>
+        </Menu.Positioner>
+      </Portal>
+    </Menu.Root>
+  );
+}
