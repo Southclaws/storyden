f401446bfd1c208171f7c699e78e22dfa1689306
Barnaby Keene <hello@southcla.ws>
2025-10-02 20:42:15 +0100

    expose error in category select loading

--- web/src/components/category/CategorySelect/CategorySelect.tsx
+++ web/src/components/category/CategorySelect/CategorySelect.tsx
@@ -1,35 +1,30 @@
 import { FieldValues } from "react-hook-form";
 
-import { Unready } from "@/components/site/Unready";
+import { ErrorTooltip } from "@/components/ui/ErrorTooltip";
 import {
   SelectField,
   SelectFieldProps,
 } from "@/components/ui/form/SelectField";
+import { HStack } from "@/styled-system/jsx";
 
 import { useCategorySelect } from "./useCategorySelect";
 
 export function CategorySelect<T extends FieldValues>(
   props: Omit<SelectFieldProps<T, any>, "collection" | "placeholder">,
 ) {
-  const { ready, collection } = useCategorySelect();
-
-  // Case 1: Categories failed to load or are still loading.
-  if (!ready) {
-    return <Unready />;
-  }
-
-  // Case 2: There are zero categories available, do not render.
-  if (collection.size === 0) {
-    return null;
-  }
+  const result = useCategorySelect();
+  const { ready, collection, error } = result;
 
   return (
-    <SelectField
-      control={props.control}
-      name={props.name}
-      disabled={!ready}
-      placeholder={ready ? "Select a category" : "Loading categories..."}
-      collection={collection}
-    />
+    <HStack gap="2" alignItems="center">
+      <SelectField
+        control={props.control}
+        name={props.name}
+        disabled={!ready}
+        placeholder={ready ? "Select a category" : "Loading categories..."}
+        collection={collection}
+      />
+      <ErrorTooltip error={error} />
+    </HStack>
   );
 }
