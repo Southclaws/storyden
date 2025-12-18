a8fb406d75abe692336eae15f5c9d68044f6f1d3
Barnaby Keene <hello@southcla.ws>
2025-12-18 21:12:09 +0000

    update next/fuma

--- home/src/app/layout.tsx
+++ home/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { RootProvider } from "fumadocs-ui/provider";
+import { RootProvider } from "fumadocs-ui/provider/next";
 import "fumadocs-ui/style.css";
 import type { ReactNode } from "react";
 
