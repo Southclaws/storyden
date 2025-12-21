faf057af2ea4712a4db43490188c8cd5851509e3
Barnaby Keene <hello@southcla.ws>
2025-12-21 16:11:46 +0000

    fix another tiptap bug with weird render order hack

--- web/src/components/content/ContentComposerRich/ContentComposerRich.tsx
+++ web/src/components/content/ContentComposerRich/ContentComposerRich.tsx
@@ -49,98 +49,98 @@ export function ContentComposerRich(props: ContentComposerProps) {
       onDragLeave={handlers.handleDragLeave}
       onDrop={handlers.handleDrop}
     >
-      {editor ? (
-        <>
-          <ComposerTools
-            enabled={!props.disabled}
-            icon={<EditIcon />}
-            workingCount={uploadingCount}
-          >
-            <EditorMenu
-              editor={editor}
-              uniqueID={`${uniqueID}-toolbar`}
-              format={format}
-              handlers={handlers}
-            />
-          </ComposerTools>
-          <BubbleMenu
+      {editor && (
+        <ComposerTools
+          enabled={!props.disabled}
+          icon={<EditIcon />}
+          workingCount={uploadingCount}
+        >
+          <EditorMenu
             editor={editor}
-            tippyOptions={{
-              placement: "bottom-start",
-              maxWidth: "100%",
-              popperOptions: {
-                modifiers: [
-                  {
-                    name: "offset",
-                    options: {
-                      offset: [0, 4],
-                    },
-                  },
-                  {
-                    name: "flip",
-                    options: {
-                      fallbackPlacements: ["top-start"],
-                      boundary: editor.view.dom,
-                      padding: 8,
-                    },
-                  },
-                  {
-                    name: "preventOverflow",
-                    options: {
-                      boundary: editor.view.dom,
-                      altAxis: true,
-                      padding: {
-                        top: 0,
-                        right: 0,
-                        // Some negative padding on the bottom allows the menu
-                        // to overflow the bottom of the editor area for cases
-                        // where the editor is only a single line. Without this,
-                        // the menu can only be placed over the text itself.
-                        bottom: -40,
-                        left: 0,
-                      },
-                      rootBoundary: "viewport",
-                      tether: false,
-                    },
-                  },
-                ],
-              },
-            }}
-            className={css({
-              zIndex: "popover",
-              borderRadius: "md",
-              display: "flex",
-              flexWrap: "wrap",
-              minW: "0",
-              maxW: "full",
-              gap: "1",
-              padding: "1",
-              backgroundColor: "bg.subtle/80",
-              backdropBlur: "frosted",
-              backdropFilter: "auto",
-              boxShadow: "md",
-            })}
-          >
-            <EditorMenu
-              editor={editor}
-              uniqueID={`${uniqueID}-menu`}
-              format={format}
-              handlers={handlers}
-            />
-          </BubbleMenu>
-          <EditorContent
-            id={`editor-content-${uniqueID}`}
-            className={css({
-              height: "full",
-              width: "full",
-            })}
-            editor={editor}
+            uniqueID={`${uniqueID}-toolbar`}
+            format={format}
+            handlers={handlers}
           />
-          <LinkPasteMenu editor={editor} />
-        </>
-      ) : (
-        <div dangerouslySetInnerHTML={{ __html: initialValueHTML }} />
+        </ComposerTools>
       )}
+      <div
+        id={`editor-content-${uniqueID}`}
+        className={css({
+          height: "full",
+          width: "full",
+        })}
+        suppressHydrationWarning
+      >
+        {editor ? (
+          <EditorContent editor={editor} />
+        ) : (
+          <div dangerouslySetInnerHTML={{ __html: initialValueHTML }} />
+        )}
+      </div>
+      {editor && (
+        <BubbleMenu
+          editor={editor}
+          tippyOptions={{
+            placement: "bottom-start",
+            maxWidth: "100%",
+            popperOptions: {
+              modifiers: [
+                {
+                  name: "offset",
+                  options: {
+                    offset: [0, 4],
+                  },
+                },
+                {
+                  name: "flip",
+                  options: {
+                    fallbackPlacements: ["top-start"],
+                    boundary: editor.view.dom,
+                    padding: 8,
+                  },
+                },
+                {
+                  name: "preventOverflow",
+                  options: {
+                    boundary: editor.view.dom,
+                    altAxis: true,
+                    padding: {
+                      top: 0,
+                      right: 0,
+                      bottom: -40,
+                      left: 0,
+                    },
+                    rootBoundary: "viewport",
+                    tether: false,
+                  },
+                },
+              ],
+            },
+          }}
+          className={css({
+            zIndex: "popover",
+            borderRadius: "md",
+            display: "flex",
+            flexWrap: "wrap",
+            minW: "0",
+            maxW: "full",
+            gap: "1",
+            padding: "1",
+            backgroundColor: "bg.subtle/80",
+            backdropBlur: "frosted",
+            backdropFilter: "auto",
+            boxShadow: "md",
+          })}
+        >
+          <EditorMenu
+            editor={editor}
+            uniqueID={`${uniqueID}-menu`}
+            format={format}
+            handlers={handlers}
+          />
+        </BubbleMenu>
+      )}
+      {editor && <LinkPasteMenu editor={editor} />}
       {isDragging && (
         <ContentDragOverlay
           isError={isDragError}
