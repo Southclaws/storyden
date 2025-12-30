72ee06d73a3e1b8ed7e1926d629b49ef6ab6a211
Barnaby Keene <hello@southcla.ws>
2025-12-30 14:07:18 +0000

    skip cache entirely for settings
    next.js does not make this easy...

--- web/src/lib/settings/settings-server.ts
+++ web/src/lib/settings/settings-server.ts
@@ -6,7 +6,13 @@ import { DefaultSettings, Settings, parseSettings } from "./settings";
 
 export async function getSettings(): Promise<Settings> {
   try {
-    const { data } = await getInfo();
+    const { data } = await getInfo({
+      cache: "default",
+      next: {
+        tags: ["settings"],
+        revalidate: 0,
+      },
+    });
     return parseSettings(data);
   } catch (e) {
     return DefaultSettings;
