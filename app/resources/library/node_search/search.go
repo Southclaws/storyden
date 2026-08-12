cc9f23d72d32df7cd12055d1bb3d707a058f3070
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-08-13 00:05:44 +0700

    refactor: drop the library search visibility option that never reached the query

--- app/resources/library/node_search/search.go
+++ app/resources/library/node_search/search.go
@@ -16,7 +16,6 @@ import (
 	"github.com/Southclaws/storyden/app/resources/library"
 	"github.com/Southclaws/storyden/app/resources/pagination"
 	"github.com/Southclaws/storyden/app/resources/tag/tag_ref"
-	"github.com/Southclaws/storyden/app/resources/visibility"
 	"github.com/Southclaws/storyden/internal/ent"
 	ent_account "github.com/Southclaws/storyden/internal/ent/account"
 	"github.com/Southclaws/storyden/internal/ent/node"
@@ -30,7 +29,6 @@ type Search interface {
 type query struct {
 	nameContains    string
 	contentContains string
-	visibility      []visibility.Visibility
 	authors         []account.AccountID
 	tags            []tag_ref.Name
 }
@@ -46,12 +44,6 @@ func WithNameContains(s string) Option {
 func WithContentContains(s string) Option {
 	return func(q *query) {
 		q.contentContains = s
-	}
-}
-
-func WithVisibility(v []visibility.Visibility) Option {
-	return func(q *query) {
-		q.visibility = v
 	}
 }
 
